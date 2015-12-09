1e9dcabd899e8e290232a76bcaf71fa97d6cd051
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-09 13:48:23 +0100

    Solve Day 9

---
+++ day09/main.hs
@@ -0,0 +1,41 @@
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.String (parseFromFile)
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.List
+import Control.Monad.Reader
+
+type City = String
+type DistanceTable = Map.Map (String, String) Int
+
+distanceTable :: Parsec String u DistanceTable
+distanceTable = Map.fromList <$> many (distance <* endOfLine) <* eof
+  where distance    = (,) <$> cities <*> (string " = " *> value)
+        cities      = (,) <$> city <*> (string " to " *> city)
+        value       = read <$> many1 digit
+        city        = (:) <$> upper <*> many letter
+
+pairs :: [a] -> [(a, a)]
+pairs x@(_:y) = zip x y
+
+distance :: (City, City) -> DistanceTable -> Int
+distance (f, t) = fmap fromJust $ mplus <$> Map.lookup (t, f) <*> Map.lookup (f, t)
+
+routeLength :: [City] -> DistanceTable -> Int
+routeLength = liftM sum . mapM distance . pairs
+
+cities :: DistanceTable -> [City]
+cities = nub . ap [fst, snd] . Map.keys . ask
+
+routeLengths :: DistanceTable -> [Int]
+routeLengths = mapM routeLength . permutations =<< cities
+
+fromRight :: Show a => Either a b -> b
+fromRight = either (error . show) id
+
+main :: IO()
+main = mainP2 where
+  mainP1 = main' minimum
+  mainP2 = main' maximum
+  main' f = parseFromFile distanceTable "input.txt" >>= print . (f <$> routeLengths) . fromRight
