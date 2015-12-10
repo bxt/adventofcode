3275c13a4a9cb8c36f5b1f76710f17d5fb845494
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-10 13:51:02 +0100

    Refactor Day 10

--- day10/main.hs
+++ day10/main.hs
@@ -1,9 +1,13 @@
 import Data.List
 
+rle :: String -> String
 rle xs = (\x -> [head $ show $ length x, head x]) =<< group xs
+
+start :: String
+start = "3113322113"
 
 main :: IO()
 main = mainP2 where
   mainP1 = main' 40
   mainP2 = main' 50
-  main' n = print $ length $ iterate rle "3113322113" !! n
+  main' n = print $ length $ iterate rle start !! n
