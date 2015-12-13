8481dea738b73dd4e45bebdc1a886665573774a6
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-13 12:26:12 +0100

    Add Ruby solution for day 1

---
+++ day01/main.rb
@@ -0,0 +1,17 @@
+File.open("input.txt", "r") do |file|
+  floor = 0
+  basementAfter = 0
+  searchingBasement = true
+  c = "x"
+
+  while c = file.getc do
+    floor += if c == '(' then 1 else -1 end
+    if searchingBasement
+      basementAfter += 1
+      searchingBasement = floor != -1;
+    end
+  end
+
+  puts "Part 1: #{floor}\nPart 2: #{basementAfter}\n"
+
+end
