01365cd7b906f3b1d410cdad0aafa4e6cfafcddc
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-15 13:37:17 +0100

    Collect both parts in C solution of day 15

--- day15/main.c
+++ day15/main.c
@@ -7,29 +7,32 @@
 
 int main(int argc, char *argv[]) {
 
-  int maxScore = 0;
+  int maxScore1 = 0;
+  int maxScore2 = 0;
 
   for (int sug = 0; sug <= 100; sug++) {
     for (int spr = 0; spr <= 100-sug; spr++) {
       for (int can = 0; can <= 100-sug-spr; can++) {
         for (int cho = 0; cho <= 100-sug-spr-can; cho++) {
           int calories = 2 * sug + 9 * spr + can + 8 * cho;
-          if (calories == 500) {
-            int capacity = sug * 3 - spr * 3 - can;
-            int durability = 3 * spr;
-            int flavor = 4 * can - 2 * cho;
-            int texture = 2 * cho - 3 * sug;
-            int score = Z(capacity) * durability * Z(flavor) * Z(texture);
-            if (score > maxScore) {
-              maxScore = score;
-            }
+          int capacity = sug * 3 - spr * 3 - can;
+          int durability = 3 * spr;
+          int flavor = 4 * can - 2 * cho;
+          int texture = 2 * cho - 3 * sug;
+          int score = Z(capacity) * durability * Z(flavor) * Z(texture);
+          if (score > maxScore1) {
+            maxScore1 = score;
+          }
+          if (calories == 500 && score > maxScore2) {
+            maxScore2 = score;
           }
         }
       }
     }
   }
 
-  printf("Max score: %d\n", maxScore);
+  printf("Max score, part 1: %d\n", maxScore1);
+  printf("Max score, part 2: %d\n", maxScore2);
 
   return 0;
 }
