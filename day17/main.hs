3fa1b3a0aaa5cd334f04381b7cf3241d7627a549
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-17 12:12:58 +0100

    Refator Day 17 a lot

--- day17/main.hs
+++ day17/main.hs
@@ -1,6 +1,5 @@
 import Control.Monad
 import Data.Ord (comparing)
-import Data.Maybe (fromJust)
 
 data Part = Part1 | Part2 deriving Show
 
@@ -12,26 +11,12 @@ minimaBy cmp = foldr aux [] where
                       EQ -> x:m:ms
                       _  ->   m:ms
 
-possibleCombinationCount :: Int -> [Int] -> Int
-possibleCombinationCount n _ | n < 0  = 0
-possibleCombinationCount n _ | n == 0 = 1
-possibleCombinationCount _ []         = 0
-possibleCombinationCount n (x:xs)     = possibleCombinationCount (n-x) xs
-                                      + possibleCombinationCount n xs
-
-possibleCombinations :: Int -> [Int] -> Maybe [[Int]]
-possibleCombinations 0 _      = Just [[]]
-possibleCombinations _ []     = Nothing
-possibleCombinations n (x:xs) | n < x     = without
-                              | otherwise = let with = liftM (map (x:)) (possibleCombinations (n-x) xs)
-                                             in cmb (++) without with
-                     where without = possibleCombinations n xs
-
-cmb :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
-cmb f (Just a) (Just b) = Just $ f a b
-cmb _ (Just a) _        = Just a
-cmb _ _        (Just b) = Just b
-cmb _ _        _        = Nothing
+possibleCombinations :: Int -> [Int] -> [[Int]]
+possibleCombinations n _ | n < 0  = []
+possibleCombinations n _ | n == 0 = [[]]
+possibleCombinations _ []         = []
+possibleCombinations n (x:xs)     = map (x:) (possibleCombinations (n-x) xs)
+                                 ++ possibleCombinations n xs
 
 eggnog :: Int
 eggnog = 150
@@ -41,8 +26,8 @@ main = do
   containers <- liftM (map read . lines) $ readFile "input.txt"
   forM_ [Part1, Part2] $ \part -> do
     print part
-    print $ calc part containers
+    print $ length $ select part $ possibleCombinations eggnog containers
 
-calc :: Part -> [Int] -> Int
-calc Part1 = possibleCombinationCount eggnog
-calc Part2 = length . minimaBy (comparing length) . fromJust . possibleCombinations eggnog
+select :: Part -> [[Int]] -> [[Int]]
+select Part1 = id
+select Part2 = minimaBy (comparing length)
