507f300e54c07b1a05a53b0611f16958c1f28760
Bernhard Häussner <bxt@die-optimisten.net>
2015-12-28 15:01:28 +0100

    Refactor Day 25

--- day25/main.hs
+++ day25/main.hs
@@ -5,11 +5,9 @@ parseInput :: String -> (Int, Int)
 parseInput s = (get 15 &&& get 17) $ words $ filter (not . isPunctuation) s
   where get n = pred . read . (!! n)
 
-cantor (x, y) = y + (((x+y)*(x+y+1)) `quot` 2)
+cantor (x, y) = y + (x + y)*(x + y + 1) `quot` 2
 
-codes = iterate step 20151125 where
-  step = (`rem` 33554393) . (* 252533) 
+codes = iterate ((`rem` 33554393) . (* 252533)) 20151125
 
 main :: IO()
-main = do
-  print =<< (codes !!) <$> cantor <$> parseInput <$> readFile "input.txt"
+main = print =<< ((codes !!) . cantor . parseInput) <$> readFile "input.txt"
