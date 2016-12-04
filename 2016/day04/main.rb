cd218ab224d518bdbbdae9b059b0e0f675f4e5c5
Bernhard Häussner <bxt@die-optimisten.net>
2016-12-04 07:20:13 +0100

    Refactor ruby solution of day 4

--- 2016/day04/main.rb
+++ 2016/day04/main.rb
@@ -1,26 +1,5 @@
-rooms = IO.readlines("input.txt").map do |line|
-  sector_id_sting, checksum = line.match(/(\d+)\[([a-z]+)\]$/).captures
-  sector_id = sector_id_sting.to_i
-  name_parts = line.scan(/(([a-z]+)-)/).map{ |match_data| match_data[1] }
-  [sector_id, checksum, name_parts]
-end
 
-valid_rooms = rooms.find_all do |(sector_id, givenChecksum, name_parts)|
-  all_letters = name_parts.reduce(:+).chars.sort
-
-  letter_counts = all_letters.each_with_object(Hash.new(0)) do |letter,counts|
-    counts[letter] += 1
-  end
-  letters_by_frequency = letter_counts.sort_by{|c|[-c[1],c[0]]}
-  most_frequent_letters = letters_by_frequency.take(5).map(&:first)
-  actualChecksum = most_frequent_letters.join
-
-  actualChecksum == givenChecksum
-end
-
-valid_checksum_sum = valid_rooms.map(&:first).reduce(:+)
-
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET = ("a".."z").to_a
 
 def rot(string, n)
   string.chars.map do |letter|
@@ -28,18 +7,58 @@ def rot(string, n)
   end.join
 end
 
-decrypted_rooms = valid_rooms.map do |(sector_id, givenChecksum, name_parts)|
-  decrypted_name_parts = name_parts.map do |name_part|
+class Room < Struct.new(:sector_id, :given_checksum, :name_parts)
+  def self.parse(room_string)
+    sector_id_sting, checksum = room_string.match(/(\d+)\[([a-z]+)\]$/).captures
+
+    sector_id = sector_id_sting.to_i
+
+    name_parts = room_string.scan(/(([a-z]+)-)/).map{ |match_data| match_data[1] }
+
+    Room.new(sector_id, checksum, name_parts)
+  end
+
+  def valid?
+    given_checksum == actual_checksum
+  end
+
+  def decrypted_name
+    decrypted_name_parts.join(' ')
+  end
+
+  def decrypted_name_parts
+    name_parts.map(&method(:decrypt_name_part))
+  end
+
+  private
+
+  def actual_checksum
+    all_letters = name_parts.reduce(:+).chars
+
+    letter_counts = all_letters.each_with_object(Hash.new(0)) do |letter,counts|
+      counts[letter] += 1
+    end
+    letters_by_frequency = letter_counts.sort_by do |letter, count|
+      [-count, letter]
+    end
+    most_frequent_letters = letters_by_frequency.take(5).map(&:first)
+
+    most_frequent_letters.join
+  end
+
+  def decrypt_name_part(name_part)
     rot(name_part, sector_id)
   end
-  [sector_id, givenChecksum, decrypted_name_parts]
 end
 
-north_pole_room = decrypted_rooms.find do |(sector_id, givenChecksum, decrypted_name_parts)|
-  decrypted_name_parts.join(' ') == "northpole object storage"
-end
+rooms = IO.readlines("input.txt").map(&Room.method(:parse))
+valid_rooms = rooms.find_all(&:valid?)
 
-id_of_north_pole_room = north_pole_room.first
+valid_checksum_sum = valid_rooms.map(&:sector_id).reduce(:+)
+
+north_pole_room = valid_rooms.find do |room|
+  room.decrypted_name == "northpole object storage"
+end
 
 puts "PartOne: #{valid_checksum_sum}"
-puts "PartTwo: #{id_of_north_pole_room}"
+puts "PartTwo: #{north_pole_room.sector_id}"
