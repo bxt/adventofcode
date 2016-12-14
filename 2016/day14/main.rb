853a873e8631ed19d1dc41d064abeae973081668
Bernhard Häussner <bxt@die-optimisten.net>
2016-12-14 13:37:50 +0100

    Try to reduce I/O in 2016:14:ruby

--- 2016/day14/main.rb
+++ 2016/day14/main.rb
@@ -1,11 +1,28 @@
 require 'digest/md5'
 
+def find_repeated_char(times, string)
+  return unless string
+  matches = string.match(/(.)\1{#{times - 1}}/)
+  return unless matches
+  matches.captures[0]
+end
+
+def stretched_md5(salt, input)
+  2017.times.reduce("#{salt}#{input}") do |prev, _|
+    Digest::MD5.hexdigest(prev)
+  end
+end
+
+def salted_md5(salt, input)
+  Digest::MD5.hexdigest("#{salt}#{input}")
+end
+
 class KeyFinder
   def initialize(key_stream)
     @key_stream = key_stream
   end
 
-  def print_key_at(index, &keygen)
+  def print_key_at(index)
     max_index_diff = 999
     max_stream_index = 333333333
     found = []
@@ -28,29 +45,17 @@ class KeyFinder
 
     puts found.sort[index]
   end
-
-  private
-
-  def find_repeated_char(times, string)
-    return unless string
-    matches = string.match(/(.)\1{#{times - 1}}/)
-    return unless matches
-    matches.captures[0]
-  end
-end
-
-def salted_md5(salt, input)
-  Digest::MD5.hexdigest("#{salt}#{input}")
 end
 
 class StretchedMD5KeyStream
   CHUNK_COUNT = 3
-  CHUNK_SIZE = 1000
+  CHUNK_SIZE = 8000
 
-  def initialize(salt)
+  def initialize(salt, &block)
     @salt = salt
     @precalculated_until = 0
-    @store = Hash.new { |h, i| raise 'not found :/' if i >= 0 }
+    @store = {}
+    @filter = block
   end
 
   def [](index)
@@ -64,10 +69,12 @@ class StretchedMD5KeyStream
       fork do
         parent_read.close
         from = @precalculated_until + i*CHUNK_SIZE
+        puts "> #{from}"
         result = {}
         CHUNK_SIZE.times do |k|
           index = from + k
-          result[index] = stretched_md5(@salt, index)
+          hash = stretched_md5(@salt, index)
+          result[index] = hash if @filter.call(hash)
         end
         Marshal.dump(result, child_write)
         exit!(0)
@@ -86,14 +93,6 @@ class StretchedMD5KeyStream
 
     @precalculated_until += CHUNK_COUNT*CHUNK_SIZE
   end
-
-  private
-
-  def stretched_md5(salt, input)
-    2017.times.reduce("#{salt}#{input}") do |prev, _|
-      Digest::MD5.hexdigest(prev)
-    end
-  end
 end
 
 salt = "abc" # -> 22728 / 22551
@@ -101,7 +100,7 @@ salt = "yjdafjpo" # -> 25427 / 22045
 
 key_streams = {
   "One" => Hash.new { |h, i| h[i] = salted_md5(salt, i) if i >= 0 },
-  "Two" => StretchedMD5KeyStream.new(salt),
+  "Two" => StretchedMD5KeyStream.new(salt) { |hash| find_repeated_char(3, hash) },
 }
 
 key_streams.each do |label, key_stream|
