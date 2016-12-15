2d6761629f57293f9d5a4d2c3db775e96252e668
Bernhard Häussner <bxt@die-optimisten.net>
2016-12-15 06:41:18 +0100

    Slight refactor of 2016:15:ruby
    Also removing each_with_index made it twice as fast.

--- 2016/day15/part1.rb
+++ 2016/day15/part1.rb
@@ -1,20 +1,20 @@
 
+DISC_PATTERN = /Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).$/
+
 discs = IO.readlines("input.txt").map do |line|
-  max, start = line.match(/Disc #\d+ has (\d+) positions; at time=0, it is at position (\d+).$/).captures
-  [max.to_i, start.to_i]
+  id, max, start = line.match(DISC_PATTERN).captures
+  [id.to_i, max.to_i, start.to_i]
 end
 
 # discs = [[5,4],[2,1]]
 
-SEARCHMAX = 1000000000
-
 def find_good_time(discs)
-  SEARCHMAX.times.find do |i|
-    discs.each_with_index.all? do |(max, start), disc_no|
-      (i + 1 + start + disc_no) % max == 0
+  (1..Float::INFINITY).find do |time|
+    discs.all? do |disc_no, max, start|
+      (time + disc_no + start) % max == 0
     end
   end
 end
 
 puts "Part One: #{find_good_time(discs)}"
-puts "Part Two: #{find_good_time(discs + [[11, 0]])}"
+puts "Part Two: #{find_good_time(discs + [[7, 11, 0]])}"
