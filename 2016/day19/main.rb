44458fd6bc7acf06458afa205ef10c3c182e853d
Bernhard Häussner <bxt@die-optimisten.net>
2016-12-19 13:17:29 +0100

    Reverse logic in 2016:19:ruby/1

--- 2016/day19/main.rb
+++ 2016/day19/main.rb
@@ -2,16 +2,16 @@
 input = 3001330 # --> 1808357
 # input = 5
 
-elves = input.times.reverse_each.to_a
+elves = input.times.to_a
 
 index = 0
 
 loop do
-  current_elf = elves.pop
+  current_elf = elves.shift
   if elves.any?
-    steal_from = elves.pop
+    steal_from = elves.shift
     # puts "#{steal_from + 1} ist raus"
-    elves.unshift(current_elf)
+    elves.push(current_elf)
   else
     puts "Elf #{current_elf + 1} gewinnt!"
     break
