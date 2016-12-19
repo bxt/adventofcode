ebdfbca99ac04d6d6b01e4e531ec6f66546d1f8e
Bernhard Häussner <bxt@die-optimisten.net>
2016-12-19 13:15:03 +0100

    Optimize 2016:19:ruby/1

--- 2016/day19/main.rb
+++ 2016/day19/main.rb
@@ -1,25 +1,19 @@
 
 input = 3001330 # --> 1808357
-#input = 5
+# input = 5
 
-elves = [true] * input
+elves = input.times.reverse_each.to_a
 
 index = 0
 
 loop do
-  while !elves[index]
-    index = (index + 1) % elves.size
-  end
-  steal_offset = (1...elves.size).find do |i|
-    elves[(index + i) % elves.size]
-  end
-  steal_from = (index + steal_offset) % elves.size if steal_offset
-  if steal_from
-    puts "#{steal_from + 1} ist raus"
-    elves[steal_from] = false
-    index += 1
+  current_elf = elves.pop
+  if elves.any?
+    steal_from = elves.pop
+    # puts "#{steal_from + 1} ist raus"
+    elves.unshift(current_elf)
   else
-    puts "Elf #{index + 1} gewinnt!"
+    puts "Elf #{current_elf + 1} gewinnt!"
     break
   end
 end
