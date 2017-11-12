cde566f2dab43cf5b1fd1db2e3bb46158a08836a
Bernhard Häussner <bxt@die-optimisten.net>
2017-11-12 20:53:06 +0100

    Add 2016:14:scala

---
+++ 2016/day14/Main.scala
@@ -0,0 +1,57 @@
+object Day14 {
+  val salt = "yjdafjpo"
+  //val salt = "abc"
+  
+  def main(args: Array[String]): Unit = {
+    // println(getMd5("iwrupvqb346386"))
+    time {
+      println(findNthKeyIndex(63, index => getMd5(salt + index))) // -> 25427
+      println(findNthKeyIndex(63, getStretchedMd5)) // -> 22045
+    }
+  }
+  
+  def findNthKeyIndex(n: Int, hasher: Int => String) = {
+    generateKeys(hasher).drop(n).next()._2
+  }
+  
+  def generateKeys(hasher: Int => String): Iterator[(String, Int)] = {
+    val hashes: Stream[String] = Stream.from(0).map(hasher)
+    
+    hashes.zipWithIndex.sliding(1001).filter(group => {
+      val (hash, index) +: next1k = group
+      
+      findRepeatedChar(3)(hash).flatMap(repeatedChar => {
+        //println(index + ": " + hash)
+        next1k.find({case (hash, index) =>
+          findRepeatedChar(5)(hash).filter(_ == repeatedChar).map(foo => {
+            println(index, hash)
+            foo
+          }).isDefined
+        })
+      }).isDefined
+    }).map(_.head)
+  }
+  
+  def findRepeatedChar(times: Int)(input: String): Option[Char] = {
+    f"(.)\\1{${times - 1}%d}".r.findFirstIn(input).map(_.charAt(0))
+  }
+  
+  def getMd5(input: String): String = {
+    val md = java.security.MessageDigest.getInstance("MD5")
+    val digest = md.digest(input.getBytes())
+    //digest.map("%02x".format(_)).mkString
+    new java.math.BigInteger(1, digest).toString(16).reverse.padTo(32, "0").reverse.mkString
+  }
+  
+  def getStretchedMd5(index: Int): String = {
+    (1 to 2017).foldLeft(salt + index)((prev, _) => getMd5(prev) )
+  }
+  
+  def time[R](block: => R): R = {  
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
+  }
+}
