2d6593be4fc72bc747322f8e7b3439ddb95ce78d
Bernhard Häussner <bxt@die-optimisten.net>
2017-12-03 12:40:15 +0100

    Add 2017:03:scala/2

--- 2017/day03/Main.scala
+++ 2017/day03/Main.scala
@@ -2,43 +2,81 @@ package day03
 
 import scala.io.Source
 
+// BTW: http://bernhardhaeussner.de/blog/26_Aufgewickelter_Zahlenstrahl
+
 object Main {
+  case class Point(x: Int, y: Int) {
+    def +(that: Point) = that match {case Point(x2, y2) => Point(x + x2, y + y2)}
+    def manhattanNorm(): Int = x.abs + y.abs
+    def mooreNeighborhood(): List[Point] = Point.eightNeighboursOfZero.map(_ + this)
+  }
+
+  // Extends is workaround for this bug: https://issues.scala-lang.org/browse/SI-3664
+  object Point extends ((Int, Int) => Point) {
+    val zero = Point(0, 0)
+    val eightNeighboursOfZero = List((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)).map(Point.tupled)
+  }
+
   def main(args: Array[String]): Unit = {
-    assert(gridCoords(16) == (-1, -2))
-    assert(gridCoords(17) == (-2, -2))
-    assert(gridCoords(18) == (-2, -1))
-    assert(gridCoords(21) == (-2, 2))
-    assert(gridCoords(22) == (-1, 2))
+    List(  9 -> ( 1,  1)
+        , 10 -> ( 2,  1)
+        , 11 -> ( 2,  0)
+        , 13 -> ( 2, -2)
+        , 14 -> ( 1, -2)
+        , 17 -> (-2, -2)
+        , 18 -> (-2, -1)
+        , 21 -> (-2,  2)
+        , 22 -> (-1,  2)
+    ) foreach { case (input, output) =>
+      assert(gridCoords(input) == Point.tupled(output))
+    }
 
     val input = 312051
-    println(partOne(input))
+    println(partOne(input)) // 430
+    println(partTwo(input)) // 312453
   }
 
   def partOne(input: Int): Int = {
-    manhattanNorm(gridCoords(input))
+    gridCoords(input).manhattanNorm
   }
 
-  def gridCoords(input: Int): (Int, Int) = {
+  def partTwo(input: Int): Int = {
+    ulamFibonaccis.find(_ > input).get
+  }
+
+  def ulamFibonaccis(): Stream[Int] = {
+    val values = scala.collection.mutable.Map(Point.zero -> 1)
+    Stream(1) #::: Stream.from(0).map(gridCoords).map(point => {
+      val sum = point.mooreNeighborhood().map(n => values.getOrElse(n, 0)).sum
+      values(point) = sum
+      sum
+    })
+  }
+
+  def gridCoords(input: Int): Point = {
     val sqrt = math.sqrt(input)
     val ring = math.floor(sqrt).toInt
     val halfRing = ring/2
     val resid = input - ring*ring
+
     if (isEven(ring)) {
       if (resid == 0) {
-        (-halfRing + 1, -halfRing)
+        Point(-halfRing + 1, -halfRing)
+      } else if(resid <= ring) {
+        Point(-halfRing, resid - halfRing - 1)
       } else {
-        if(resid <= ring) {
-          (-halfRing, resid - halfRing - 1)
-        } else {
-          (-halfRing + resid - ring - 1, halfRing)
-        }
+        Point(-halfRing + resid - ring - 1, halfRing)
       }
     } else {
-      throw new NotImplementedError
+      if (resid == 0) {
+        Point(halfRing, halfRing)
+      } else if (resid <= ring) {
+        Point(halfRing + 1, halfRing + 1 - resid)
+      } else {
+        Point(halfRing - resid + ring + 2, -halfRing - 1)
+      }
     }
   }
 
   def isEven(number: Int) = number % 2 == 0
-
-  def manhattanNorm(t: (Int, Int)) = t match { case (a, b) => a.abs + b.abs }
 }
