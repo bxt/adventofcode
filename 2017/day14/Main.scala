fcd5a1fd852e695a43330616feeeeef12e339c4a
Bernhard Häussner <bxt@die-optimisten.net>
2017-12-14 21:34:59 +0100

    Change some other stuff about 2017:14:scala

--- 2017/day14/Main.scala
+++ 2017/day14/Main.scala
@@ -13,8 +13,6 @@ import day12.Main.connectedComponents
 
 object Main {
   case class Grid(values: Seq[Seq[Boolean]]) {
-    def count: Int = values.map(bin => bin.count(identity)).sum
-
     def pointsAndValues: Seq[Seq[(Point, Boolean)]] = {
       values.zipWithIndex.map({
         case (rowValues, row) => {
@@ -23,10 +21,9 @@ object Main {
       })
     }
 
-    def points: Set[Point] = pointsAndValues.flatten.filter(_._2).map(_._1).toSet
+    lazy val points: Set[Point] = pointsAndValues.flatten.filter(_._2).map(_._1).toSet
 
-    def components: Set[Set[Point]] = {
-      val points = this.points
+    lazy val components: Set[Set[Point]] = {
       val links = points.flatMap(p => (p.neighborhood(VonNeumann).toSet & points | Set(p)).map(n => (p, n)))
       connectedComponents(links)
     }
@@ -37,8 +34,8 @@ object Main {
       pointsAndValues.map(
         _.map({
           case (p, value) => if (value) {
-            val ccNum: BigInt = ccSeq.indexWhere(_(p))
-            ccNum.toString(36).padTo(3, ' ')
+            val ccNum = ccSeq.indexWhere(_(p))
+            BigInt(ccNum).toString(36).padTo(3, ' ')
           } else " . "
         }).mkString).mkString("\n")
     }
@@ -59,12 +56,12 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val exampleGrid = Grid.generate("flqrgnkx")
-    assert(exampleGrid.count == 8108)
+    assert(exampleGrid.points.size == 8108)
     assert(exampleGrid.components.size == 1242)
 
     val input = "ljoxqyyw"
     val inputGrid = Grid.generate(input)
-    println(inputGrid.count) // 8316
+    println(inputGrid.points.size) // 8316
     println(inputGrid.components.size) // 1074
     println(inputGrid.mkString)
   }
