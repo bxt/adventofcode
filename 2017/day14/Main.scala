d6ba62c652b04ce4dac1c007f27bbf0e1ca1c4ba
Bernhard Häussner <bxt@die-optimisten.net>
2017-12-14 21:10:01 +0100

    Refactor 2017:14:scala

--- 2017/day14/Main.scala
+++ 2017/day14/Main.scala
@@ -7,72 +7,67 @@ import scala.annotation.tailrec
 import day03.Main.Point
 import day03.Main.Point.VonNeumann
 
-object Main {
-  type Grid = Seq[Seq[Boolean]]
+import day10.Main.knotHash
 
-  val GRID_SIZE = 128
+import day12.Main.connectedComponents
+
+object Main {
+  case class Grid(values: Seq[Seq[Boolean]]) {
+    def count: Int = values.map(bin => bin.count(identity)).sum
+
+    def pointsAndValues: Seq[Seq[(Point, Boolean)]] = {
+      values.zipWithIndex.map({
+        case (rowValues, row) => {
+          rowValues.zipWithIndex.map({ case (value, col) => (Point(row, col), value) })
+        }
+      })
+    }
+
+    def points: Set[Point] = pointsAndValues.flatten.filter(_._2).map(_._1).toSet
+
+    def components: Set[Set[Point]] = {
+      val points = this.points
+      val links = points.flatMap(p => (p.neighborhood(VonNeumann).toSet & points | Set(p)).map(n => (p, n)))
+      connectedComponents(links)
+    }
+
+    def mkString: String = {
+      val ccSeq = components.toSeq
+
+      pointsAndValues.map(
+        _.map({
+            case (p, value) => if (value) {
+              val ccNum: BigInt = ccSeq.indexWhere(_(p))
+              ccNum.toString(36).padTo(3, ' ')
+            } else " . "
+          }).mkString
+      ).mkString("\n")
+    }
+  }
+
+  object Grid {
+    val SIZE = 128
+
+    def generate(input: String): Grid = {
+      Grid(0.until(SIZE)
+        .map(n => f"$input%s-$n%d")
+        .map(knotHash(_))
+        .map(h => BigInt(h, 16).toString(2))
+        .map(binStr => binStr.map({ case '0' => false; case '1' => true }))
+        .map(binStr => binStr.reverse.padTo(SIZE, false).reverse))
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    val example = "flqrgnkx"
-    val exampleGrid = generateGrid(example)
-    //printGrid(exampleGrid)
-    val exampleGridIndices = gridPoints(exampleGrid)
-    assert(exampleGridIndices(Point(0, 0)))
-    assert(exampleGridIndices(Point(0, 1)))
-    assert(!exampleGridIndices(Point(1, 0)))
-    assert(exampleGridIndices(Point(1, 1)))
-    assert(countGrid(exampleGrid) == 8108)
-    println("Well..?")
-    val ccs = gridCcs(exampleGrid)
-    printGridWithCcs(exampleGrid, ccs)
-    assert(ccs.size == 1242)
+    val exampleGrid = Grid.generate("flqrgnkx")
+    assert(exampleGrid.count == 8108)
+    assert(exampleGrid.components.size == 1242)
 
     val input = "ljoxqyyw"
-    val inputGrid = generateGrid(input)
-    println(countGrid(inputGrid))
-    println(gridCcs(inputGrid).size)
-  }
-
-  def generateGrid(input: String): Grid = {
-    0
-        .until(GRID_SIZE)
-        .map(n => f"$input%s-$n%d")
-        .map(day10.Main.knotHash(_))
-        .map(h => BigInt(h, 16).toString(2))
-        .map(binStr => binStr.map({ case '0' => false; case '1' => true}).reverse.padTo(GRID_SIZE, false).reverse)
-  }
-
-  def printGrid(grid: Grid): Unit = {
-    println(grid.map(bin => bin.map(x => if (x) '#' else '.').mkString).mkString("\n"))
-  }
-
-  def printGridWithCcs(grid: Grid, ccs: Set[Set[Point]]): Unit = {
-    val ccSeq = ccs.toSeq
-
-    println(grid.zipWithIndex.map({ case (rowValues, row) =>
-      rowValues.zipWithIndex.map({ case (value, col) => if (value) {
-        val ccNum = ccSeq.indexWhere(_(Point(row, col)))
-        val x: BigInt = ccNum
-        x.toString(36).padTo(3, ' ')
-      } else " . "}).mkString
-    }).mkString("\n"))
-  }
-
-  def countGrid(grid: Grid): Int = {
-    grid.map(bin => bin.count(identity)).sum
-  }
-
-  def gridPoints(grid: Grid): Set[Point] = {
-    grid.zipWithIndex.flatMap({ case (rowValues, row) =>
-      rowValues.zipWithIndex.flatMap({ case (value, col) => if (value) Some(Point(row, col)) else None })
-    }).toSet
-  }
-
-  def gridCcs(grid: Grid): Set[Set[Point]] = {
-    val points = gridPoints(grid)
-    val links = points.flatMap(p => (p.neighborhood(VonNeumann).toSet & points | Set(p)).map(n => (p, n)))
-    println(links.filter(ps => ps._1.x < 5 && ps._1.y < 5 && ps._2.x < 5 && ps._2.y < 5 ))
-    day12.Main.connectedComponents(links)
+    val inputGrid = Grid.generate(input)
+    println(inputGrid.count) // 8316
+    println(inputGrid.components.size) // 1074
+    println(inputGrid.mkString)
   }
 }
 
