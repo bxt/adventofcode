505aaf30c90317e139c88f247cc49d6278a38328
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-05 18:22:30 +0100

    feat: add 2020 day 05 in TypeScript

---
+++ 2020/day05/main.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env deno run --allow-read
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+
+const range = (from: number, to: number): number[] =>
+  Array(to - from + 1).fill(true).map((_, i) => i + from);
+
+assertEquals(range(2, 5), [2, 3, 4, 5]);
+
+const getSeatId = (seat: string): number =>
+  parseInt(
+    seat.replaceAll("F", "0").replaceAll("B", "1").replaceAll("L", "0")
+      .replaceAll("R", "1"),
+    2,
+  );
+
+const testSeatId = (seat: string, expectedSeatId: number) => {
+  assertEquals(getSeatId(seat), expectedSeatId, seat);
+};
+
+testSeatId("FBFBBFFRLR", 357);
+testSeatId("BFFFBBFRRR", 567);
+testSeatId("FFFBBBFRRR", 119);
+testSeatId("BBFFBBFRLL", 820);
+
+const input = await Deno.readTextFile("input.txt");
+
+const seatIds = new Set(input.trim().split("\n").map(getSeatId));
+
+const minSeatId = Math.min(...seatIds);
+const maxSeatId = Math.max(...seatIds);
+
+console.log("Result part 1: " + maxSeatId);
+
+const missingSeatIds = range(minSeatId, maxSeatId).filter((seatId) =>
+  !seatIds.has(seatId)
+);
+
+assertEquals(missingSeatIds.length, 1);
+
+console.log("Result part 1: " + missingSeatIds[0]);
