db81942eb38d1f86faa5c7c59d4d858d56f5e471
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-07 17:16:14 +0100

    feat(2020/07/TypeScript): solve day 7 🎄

---
+++ 2020/day07/main.ts
@@ -0,0 +1,115 @@
+#!/usr/bin/env deno run --allow-read
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+
+const sum = (numbers: number[]) =>
+  numbers.reduce((acc, number) => acc + number, 0);
+
+type Bag = {
+  color: string;
+  contents: { color: string; quantity: number }[];
+};
+
+const parseInput = (string: string): Bag[] =>
+  [...string.matchAll(
+    /\W*(?<color>.*?) bags contain (?<contentsMatch>[^.]+)\./g,
+  )]
+    .map(({ groups }) => {
+      if (!groups) throw new Error("?");
+      const { color, contentsMatch } = groups;
+
+      const contents = [
+        ...contentsMatch.matchAll(/(?<quantityMatch>\d+) (?<color>.*?) bag/g),
+      ].map(({ groups }) => {
+        if (!groups) throw new Error("?");
+        const { quantityMatch, color } = groups;
+
+        return {
+          color,
+          quantity: Number(quantityMatch),
+        };
+      });
+
+      return {
+        color,
+        contents,
+      };
+    });
+
+const example = parseInput(`
+  light red bags contain 1 bright white bag, 2 muted yellow bags.
+  dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+  bright white bags contain 1 shiny gold bag.
+  muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+  shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+  dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+  vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+  faded blue bags contain no other bags.
+  dotted black bags contain no other bags.
+`);
+
+assertEquals(
+  example[0],
+  {
+    color: "light red",
+    contents: [
+      { quantity: 1, color: "bright white" },
+      { quantity: 2, color: "muted yellow" },
+    ],
+  },
+);
+
+const BEAUTIFUL_BAG = "shiny gold";
+
+const input = await Deno.readTextFile("input.txt");
+
+const inputParsed = parseInput(input);
+
+const directPossibilities = (bags: Bag[], color: string): Bag[] =>
+  bags.filter((bag) => bag.contents.some((c) => c.color === color));
+
+const recursivePossibilities = (bags: Bag[], color: string): Bag[] => {
+  const direct = directPossibilities(bags, color);
+  const forContents = direct.flatMap(({ color }) =>
+    recursivePossibilities(bags, color)
+  );
+
+  return [...direct, ...forContents];
+};
+
+const part1 = (bags: Bag[]): number =>
+  new Set(
+    [...recursivePossibilities(bags, BEAUTIFUL_BAG).map((b) => b.color)],
+  ).size;
+
+assertEquals(part1(example), 4);
+
+console.log("Result part 1: " + part1(inputParsed));
+
+const recurseContents = (bags: Bag[], color: string): number => {
+  const bag = bags.find((bag) => bag.color === color);
+  if (!bag) throw new Error(`Referenced unknonw color ${color}`);
+
+  return sum(
+    bag.contents.map(({ color, quantity }) =>
+      quantity * (1 + recurseContents(bags, color))
+    ),
+  );
+};
+
+const part2 = (bags: Bag[]): number => recurseContents(bags, BEAUTIFUL_BAG);
+
+assertEquals(part2(example), 32);
+
+const example2 = parseInput(`
+  shiny gold bags contain 2 dark red bags.
+  dark red bags contain 2 dark orange bags.
+  dark orange bags contain 2 dark yellow bags.
+  dark yellow bags contain 2 dark green bags.
+  dark green bags contain 2 dark blue bags.
+  dark blue bags contain 2 dark violet bags.
+  dark violet bags contain no other bags.
+`);
+
+assertEquals(part2(example2), 126);
+
+console.log("Result part 2: " + part2(inputParsed));
