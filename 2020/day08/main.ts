fc20f8cc885124bdb75b4994733ae06c693c2352
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-08 06:35:20 +0100

    refactor(2020/08/TypeScript): make the code a bit nicer

--- 2020/day08/main.ts
+++ 2020/day08/main.ts
@@ -1,48 +1,48 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
 
-type Command = { command: "nop" | "acc" | "jmp"; number: number };
+type Instruction = { operation: "nop" | "acc" | "jmp"; argument: number };
 
-const parseInput = (string: string): Command[] =>
+const parseInput = (string: string): Instruction[] =>
   [...string.matchAll(
-    /\W*(?<command>.*?) (?<number>[+-]\d+)/g,
+    /\W*(?<operation>.*?) (?<argument>[+-]\d+)/g,
   )]
     .map(({ groups }) => {
       if (!groups) throw new Error("?");
-      const { command, number } = groups;
+      const { operation, argument } = groups;
 
-      if (command !== "nop" && command !== "acc" && command !== "jmp") {
-        throw new Error(command);
+      if (operation !== "nop" && operation !== "acc" && operation !== "jmp") {
+        throw new Error(`Invalid operation: ${operation}`);
       }
 
-      return { command, number: Number(number) };
+      return { operation, argument: Number(argument) };
     });
 
 const example = parseInput(`
-nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6
+  nop +0
+  acc +1
+  jmp +4
+  acc +3
+  jmp -3
+  acc -99
+  acc +1
+  jmp -4
+  acc +6
 `);
 
 assertEquals(
   example[0],
   {
-    command: "nop",
-    number: 0,
+    operation: "nop",
+    argument: 0,
   },
 );
 
 assertEquals(
   example[1],
   {
-    command: "acc",
-    number: 1,
+    operation: "acc",
+    argument: 1,
   },
 );
 
@@ -50,59 +50,65 @@ const input = await Deno.readTextFile("input.txt");
 
 const inputParsed = parseInput(input);
 
-const part1 = (commands: Command[]): number => {
-  let acc = 0;
-  const seen = new Set();
-  let index = 0;
-  while (!seen.has(index)) {
-    seen.add(index);
-    const { command, number } = commands[index];
-    if (command === "nop") {
-      index++;
-    } else if (command === "acc") {
-      acc += number;
-      index++;
-    } else if (command === "jmp") {
-      index += number;
+type RunResult = {
+  infiniteLoop: boolean;
+  accumulatorValue: number;
+};
+
+const runInstructions = (instructions: Instruction[]): RunResult => {
+  let accumulatorValue = 0;
+  const seenInstructionPointers = new Set();
+  let instructionPointer = 0;
+  while (instructionPointer < instructions.length) {
+    if (seenInstructionPointers.has(instructionPointer)) {
+      return { infiniteLoop: true, accumulatorValue };
+    }
+    seenInstructionPointers.add(instructionPointer);
+    const { operation, argument } = instructions[instructionPointer];
+    if (operation === "nop") {
+      instructionPointer++;
+    } else if (operation === "acc") {
+      accumulatorValue += argument;
+      instructionPointer++;
+    } else if (operation === "jmp") {
+      instructionPointer += argument;
     }
   }
-  return acc;
+  return { infiniteLoop: false, accumulatorValue };
+};
+
+const part1 = (instructions: Instruction[]): number => {
+  const { infiniteLoop, accumulatorValue } = runInstructions(instructions);
+  if (!infiniteLoop) throw new Error("Expected inifnite loop!");
+  return accumulatorValue;
 };
 
 assertEquals(part1(example), 5);
 
 console.log("Result part 1: " + part1(inputParsed));
 
-const part2 = (commands: Command[]): number => {
-  xx:
-  for (let i = 0; i < commands.length; i++) {
-    if (commands[i].command === "acc") continue xx;
-    if (commands[i].command === "nop") commands[i].command = "jmp";
-    else commands[i].command = "nop";
-
-    let acc = 0;
-    const seen = new Set();
-    let index = 0;
-    while (index < commands.length) {
-      if (seen.has(index)) {
-        if (commands[i].command === "nop") commands[i].command = "jmp";
-        else commands[i].command = "nop";
-        continue xx;
-      }
-      seen.add(index);
-      const { command, number } = commands[index];
-      if (command === "nop") {
-        index++;
-      } else if (command === "acc") {
-        acc += number;
-        index++;
-      } else if (command === "jmp") {
-        index += number;
-      }
-    }
-    return acc;
+const switchInstructions = (
+  instructions: Instruction[],
+  index: number,
+): void => {
+  if (instructions[index].operation === "nop") {
+    instructions[index].operation = "jmp";
+  } else if (instructions[index].operation === "jmp") {
+    instructions[index].operation = "nop";
   }
-  throw new Error("hm.");
+};
+
+const part2 = (instructions: Instruction[]): number => {
+  for (let i = 0; i < instructions.length; i++) {
+    switchInstructions(instructions, i);
+    const { infiniteLoop, accumulatorValue } = runInstructions(instructions);
+    switchInstructions(instructions, i);
+
+    if (!infiniteLoop) {
+      return accumulatorValue;
+    }
+  }
+  throw new Error("Always ran into infinite loops :/");
 };
 
 assertEquals(part2(example), 8);
