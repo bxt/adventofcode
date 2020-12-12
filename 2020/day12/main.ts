29359ad58c170dfe0b3a20ac02135783a64fb883
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-12 08:28:49 +0100

    refactor(2020/12/TypeScript): merge both parts and clean up code

--- 2020/day12/main.ts
+++ 2020/day12/main.ts
@@ -1,44 +1,35 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import {
+  addCoords,
+  Coord,
+  ensureElementOf,
+  manhattanNormCoord,
+  matchGroups,
+  rotateLeftNinetyDegreesCoord,
+  scaleCoord,
+} from "../utils.ts";
 
-const sum = (numbers: number[]): number =>
-  numbers.reduce((acc, number) => acc + number, 0);
+const actions = ["N", "S", "E", "W", "L", "R", "F"] as const;
 
-type Coord = readonly [number, number];
+type Instruction = { action: typeof actions[number]; number: number };
 
-const addCoords = (
-  [x1, y1]: Coord,
-  [x2, y2]: Coord,
-): Coord => ([x1 + x2, y1 + y2]);
+const E: Coord = [1, 0];
+const N: Coord = [0, 1];
+const W: Coord = [-1, 0];
+const S: Coord = [0, -1];
+const headings = { E, N, W, S } as const;
 
-const scaleCoord = ([x, y]: Coord, s: number): Coord => ([x * s, y * s]);
-
-const manhattanNormCoord = ([x, y]: Coord): number => Math.abs(x) + Math.abs(y);
-
-type Instruction = { letter: string; number: number };
-
-const east: Coord = [1, 0];
-const north: Coord = [0, -1];
-const west: Coord = [-1, 0];
-const south: Coord = [0, 1];
-const possibleHeadings: Coord[] = [east, north, west, south];
-
-type Ship = { heading: number; position: Coord };
+type Ship = { waypoint: Coord; position: Coord };
 
 const parseInput = (string: string): Instruction[] =>
-  string.trim().split(/[\n ]+/)
-    .map((line) => {
-      const groups = line.match(
-        /(?<letter>[A-Z])(?<number>\d+)/,
-      )?.groups;
-
-      if (!groups) throw new Error(`Did not match: ${line}`);
-
-      return {
-        letter: groups.letter,
-        number: Number(groups.number),
-      };
-    });
+  string.trim().split(/[\n ]+/).map(
+    matchGroups(/(?<letter>[A-Z])(?<number>\d+)/),
+  )
+    .map((groups) => ({
+      action: ensureElementOf(groups.letter, actions),
+      number: Number(groups.number),
+    }));
 
 const example = parseInput(`
   F10
@@ -48,8 +39,8 @@ const example = parseInput(`
   F11
 `);
 
-assertEquals(example[0], { letter: "F", number: 10 });
-assertEquals(example[1], { letter: "N", number: 3 });
+assertEquals(example[0], { action: "F", number: 10 });
+assertEquals(example[1], { action: "N", number: 3 });
 assertEquals(example.length, 5);
 
 const input = await Deno.readTextFile("input.txt");
@@ -69,70 +60,95 @@ const degreesToAmount = (degrees: number): number => {
   throw new Error(`Unimplemented amount of degrees: ${degrees}`);
 };
 
-const walkOne = (
-  ship: Ship,
-  instruction: Instruction,
-): Ship => {
-  const { heading, position } = ship;
-  const { letter, number } = instruction;
-  console.log({ heading, position, letter, number });
-  if (letter === "N") {
-    return {
-      heading,
-      position: addCoords(position, scaleCoord(north, number)),
-    };
-  }
-  if (letter === "S") {
-    return {
-      heading,
-      position: addCoords(position, scaleCoord(south, number)),
-    };
-  }
-  if (letter === "E") {
-    return {
-      heading,
-      position: addCoords(position, scaleCoord(east, number)),
-    };
-  }
-  if (letter === "W") {
-    return {
-      heading,
-      position: addCoords(position, scaleCoord(west, number)),
-    };
-  }
-  if (letter === "L" || letter === "R") {
-    const sign = letter === "L" ? 1 : -1;
-    const amount = degreesToAmount(number);
-    return {
-      heading: (possibleHeadings.length + heading + amount * sign) %
-        possibleHeadings.length,
-      position,
-    };
-  }
-  if (letter === "F") {
-    return {
-      heading,
-      position: addCoords(
-        position,
-        scaleCoord(possibleHeadings[heading], number),
-      ),
-    };
-  }
+const walkOne = (moveShipOrWaypoint: "ship" | "waypoint") =>
+  (ship: Ship, instruction: Instruction): Ship => {
+    const { waypoint, position } = ship;
+    const { action, number } = instruction;
+    switch (action) {
+      case "N":
+      case "S":
+      case "E":
+      case "W": {
+        const offset = headings[action];
+        return moveShipOrWaypoint === "ship"
+          ? {
+            position: addCoords(position, scaleCoord(offset, number)),
+            waypoint,
+          }
+          : {
+            position,
+            waypoint: addCoords(waypoint, scaleCoord(offset, number)),
+          };
+      }
+      case "L":
+      case "R": {
+        const sign = action === "L" ? 1 : -1;
+        const amount = degreesToAmount(number);
+        const numberOfHeadings = Object.keys(headings).length;
+        const leftTurns = (numberOfHeadings + amount * sign) % numberOfHeadings;
+        return {
+          waypoint: Array(leftTurns).fill(0).reduce(
+            rotateLeftNinetyDegreesCoord,
+            waypoint,
+          ),
+          position,
+        };
+      }
+      case "F":
+        return {
+          waypoint,
+          position: addCoords(
+            position,
+            scaleCoord(waypoint, number),
+          ),
+        };
+    }
+  };
 
-  throw new Error(`Unknown command: ${letter}`);
+const walkAllPart1 = (instructions: Instruction[]): Ship => {
+  const initialShip: Ship = { waypoint: headings.E, position: [0, 0] };
+
+  return instructions.reduce(walkOne("ship"), initialShip);
 };
 
-const walkAll = (instructions: Instruction[]): Ship => {
-  const initialShip: Ship = { heading: 0, position: [0, 0] };
-
-  return instructions.reduce(walkOne, initialShip);
-};
-
-assertEquals(walkAll(example), { heading: 3, position: [17, 8] });
+assertEquals(
+  walkAllPart1(example),
+  {
+    waypoint: headings.S,
+    position: addCoords(scaleCoord(headings.E, 17), scaleCoord(headings.S, 8)),
+  },
+);
 
 const part1 = (instructions: Instruction[]): number =>
-  manhattanNormCoord(walkAll(instructions).position);
+  manhattanNormCoord(walkAllPart1(instructions).position);
 
 assertEquals(part1(example), 25);
 
 console.log("Result part 1: " + part1(inputParsed));
+
+const walkAllPart2 = (instructions: Instruction[]): Ship => {
+  const initialShip: Ship = {
+    waypoint: addCoords(scaleCoord(headings.E, 10), headings.N),
+    position: [0, 0],
+  };
+
+  return instructions.reduce(walkOne("waypoint"), initialShip);
+};
+
+assertEquals(
+  walkAllPart2(example),
+  {
+    waypoint: addCoords(scaleCoord(headings.E, 4), scaleCoord(headings.S, 10)),
+    position: addCoords(
+      scaleCoord(headings.E, 214),
+      scaleCoord(headings.S, 72),
+    ),
+  },
+);
+
+const part2 = (instructions: Instruction[]): number =>
+  manhattanNormCoord(walkAllPart2(instructions).position);
+
+assertEquals(part2(example), 286);
+
+console.log("Result part 2: " + part2(inputParsed));
