0cb6da5b3b372abbd3bd21bb7a757de2aabc2d08
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-17 09:11:14 +0100

    feat(2020/17/TypeScript): solve part 2, the easy way :)
    This also reverts commit a208261a5cc78263ad3cc4c5f78e98c9f60a2dec.

---
+++ 2020/day17/part1.ts
@@ -0,0 +1,130 @@
+#!/usr/bin/env deno run --allow-read
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import { ensureElementOf, sum } from "../utils.ts";
+
+// SUSI OFFICIALLY ENTERED THE BUILDING! https://www.twitch.tv/veloxxmusic
+
+const ACTIVE = "#";
+const INACTIVE = ".";
+type Entry = typeof ACTIVE | typeof INACTIVE;
+
+const parseInput = (string: string): Entry[][] =>
+  string.trim().split("\n").map((line) => (
+    line.trim().split("").map((letter) =>
+      ensureElementOf(letter, [ACTIVE, INACTIVE] as const)
+    )
+  ));
+
+const input = await Deno.readTextFile("input.txt");
+
+const inputParsed = parseInput(input);
+
+type Trinion = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+const example = parseInput(`
+  .#.
+  ..#
+  ###
+`);
+
+const offsets = [1, 0, -1];
+const neighbours = offsets.flatMap((z) =>
+  offsets.flatMap((y) => offsets.map((x) => ({ x, y, z })))
+).filter(({ x, y, z }) => !(x === 0 && y === 0 && z === 0));
+
+assertEquals(neighbours.length, 26);
+
+const getValueAt = (pocketDimension: Entry[][][], trinion: Trinion): Entry => {
+  const { x, y, z } = trinion;
+  if (z < 0 || z >= pocketDimension.length) return INACTIVE;
+  if (y < 0 || y >= pocketDimension[z].length) return INACTIVE;
+  if (x < 0 || x >= pocketDimension[z][y].length) return INACTIVE;
+  return pocketDimension[z][y][x];
+};
+
+const next = (pocketDimension: Entry[][][]): Entry[][][] => {
+  const result: Entry[][][] = [];
+  for (let z = -1; z < pocketDimension.length + 1; z++) {
+    const slice: Entry[][] = [];
+    for (let y = -1; y < pocketDimension[0].length + 1; y++) {
+      const row: Entry[] = [];
+      for (let x = -1; x < pocketDimension[0][0].length + 1; x++) {
+        const activeNeighbours = neighbours.map(({ x: xn, y: yn, z: zn }) => ({
+          x: x + xn,
+          y: y + yn,
+          z: z + zn,
+        })).map((t) => getValueAt(pocketDimension, t)).filter((e) =>
+          e === ACTIVE
+        ).length;
+        const currentValue = getValueAt(pocketDimension, { x, y, z });
+        const nextValue = currentValue === ACTIVE
+          ? (activeNeighbours === 2 || activeNeighbours === 3)
+            ? ACTIVE
+            : INACTIVE
+          : (activeNeighbours === 3)
+          ? ACTIVE
+          : INACTIVE;
+        row.push(nextValue);
+      }
+      slice.push(row);
+    }
+    result.push(slice);
+  }
+  return result;
+};
+
+assertEquals(
+  next([example])[0],
+  parseInput(`
+    .....
+    .....
+    .#...
+    ...#.
+    ..#..
+  `),
+);
+
+assertEquals(
+  next([example])[1],
+  parseInput(`
+    .....
+    .....
+    .#.#.
+    ..##.
+    ..#..
+  `),
+);
+
+assertEquals(
+  next([example])[2],
+  parseInput(`
+    .....
+    .....
+    .#...
+    ...#.
+    ..#..
+  `),
+);
+
+const part1 = (input: Entry[][]) => {
+  const finalPocketDimension = Array(6).fill(null).reduce(
+    (pocketDimension: Entry[][][], _) => next(pocketDimension),
+    [input],
+  );
+  return sum(
+    finalPocketDimension.flatMap((slice) =>
+      slice.flatMap((row) => row.flatMap((e) => e === ACTIVE ? 1 : 0))
+    ),
+  );
+};
+
+assertEquals(part1(example), 112);
+
+console.log("Result part 1: " + part1(inputParsed));
---
+++ 2020/day17/part2.ts
@@ -0,0 +1,147 @@
+#!/usr/bin/env deno run --allow-read
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import { ensureElementOf, sum } from "../utils.ts";
+
+// SUZI OFFICIALLY ENTERED THE BUILDING! https://www.twitch.tv/veloxxmusic
+
+const ACTIVE = "#";
+const INACTIVE = ".";
+type Entry = typeof ACTIVE | typeof INACTIVE;
+
+const parseInput = (string: string): Entry[][] =>
+  string.trim().split("\n").map((line) => (
+    line.trim().split("").map((letter) =>
+      ensureElementOf(letter, [ACTIVE, INACTIVE] as const)
+    )
+  ));
+
+const input = await Deno.readTextFile("input.txt");
+
+const inputParsed = parseInput(input);
+
+type Quaternion = {
+  x: number;
+  y: number;
+  z: number;
+  w: number;
+};
+
+const example = parseInput(`
+  .#.
+  ..#
+  ###
+`);
+
+const offsets = [1, 0, -1];
+const neighbours = offsets.flatMap((w) =>
+  offsets.flatMap((z) =>
+    offsets.flatMap((y) => offsets.map((x) => ({ x, y, z, w })))
+  )
+).filter(({ x, y, z, w }) => !(x === 0 && y === 0 && z === 0 && w === 0));
+
+assertEquals(neighbours.length, 80);
+
+const getValueAt = (
+  pocketDimension: Entry[][][][],
+  quaternion: Quaternion,
+): Entry => {
+  const { x, y, z, w } = quaternion;
+  if (w < 0 || w >= pocketDimension.length) return INACTIVE;
+  if (z < 0 || z >= pocketDimension[w].length) return INACTIVE;
+  if (y < 0 || y >= pocketDimension[w][z].length) return INACTIVE;
+  if (x < 0 || x >= pocketDimension[w][z][y].length) return INACTIVE;
+  return pocketDimension[w][z][y][x];
+};
+
+const next = (pocketDimension: Entry[][][][]): Entry[][][][] => {
+  const result: Entry[][][][] = [];
+  for (let w = -1; w < pocketDimension.length + 1; w++) {
+    const hyperspace: Entry[][][] = [];
+    for (let z = -1; z < pocketDimension[0].length + 1; z++) {
+      const slice: Entry[][] = [];
+      for (let y = -1; y < pocketDimension[0][0].length + 1; y++) {
+        const row: Entry[] = [];
+        for (let x = -1; x < pocketDimension[0][0][0].length + 1; x++) {
+          const activeNeighbours =
+            neighbours.map(({ x: xn, y: yn, z: zn, w: wn }) => ({
+              x: x + xn,
+              y: y + yn,
+              z: z + zn,
+              w: w + wn,
+            })).map((t) => getValueAt(pocketDimension, t)).filter((e) =>
+              e === ACTIVE
+            ).length;
+          const currentValue = getValueAt(pocketDimension, { x, y, z, w });
+          const nextValue = currentValue === ACTIVE
+            ? (activeNeighbours === 2 || activeNeighbours === 3)
+              ? ACTIVE
+              : INACTIVE
+            : (activeNeighbours === 3)
+            ? ACTIVE
+            : INACTIVE;
+          row.push(nextValue);
+        }
+        slice.push(row);
+      }
+      hyperspace.push(slice);
+    }
+    result.push(hyperspace);
+  }
+  return result;
+};
+
+console.log(next([[example]]));
+
+assertEquals(
+  next([[example]])[1][0],
+  parseInput(`
+    .....
+    .....
+    .#...
+    ...#.
+    ..#..
+  `),
+);
+
+assertEquals(
+  next([[example]])[1][1],
+  parseInput(`
+    .....
+    .....
+    .#.#.
+    ..##.
+    ..#..
+  `),
+);
+
+assertEquals(
+  next([[example]])[1][2],
+  parseInput(`
+    .....
+    .....
+    .#...
+    ...#.
+    ..#..
+  `),
+);
+
+const part2 = (input: Entry[][]) => {
+  const finalPocketDimension = Array(6).fill(null).reduce(
+    (pocketDimension: Entry[][][][], _) => next(pocketDimension),
+    [[input]],
+  );
+  return sum(
+    finalPocketDimension.flatMap((hyperspace) =>
+      hyperspace.flatMap((slice) =>
+        slice.flatMap((row) => row.flatMap((e) => e === ACTIVE ? 1 : 0))
+      )
+    ),
+  );
+};
+
+assertEquals(part2(example), 848);
+
+console.log("Result part 2: " + part2(inputParsed));
