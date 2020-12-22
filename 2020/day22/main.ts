81a0984ea2cbcc2568645d8fcf34bfcb85a05301
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-22 08:13:09 +0100

    refactor(2020/22/TypeScript): remove debug output and refactor

--- 2020/day22/main.ts
+++ 2020/day22/main.ts
@@ -3,7 +3,7 @@ import {
   assert,
   assertEquals,
 } from "https://deno.land/std@0.79.0/testing/asserts.ts";
-import { intersectSets, matchGroups, minusSets, sum } from "../utils.ts";
+import { matchGroups, sum } from "../utils.ts";
 
 type Player = 1 | 2;
 type Card = number;
@@ -90,73 +90,44 @@ assertEquals(example, parseInput(stringifyGame(example)));
 
 const part2 = (
   game: Game,
-  gameNumber?: { n: number },
 ): { winner: Player; score: number } => {
   const seen: Set<string> = new Set();
 
-  gameNumber ||= { n: 0 };
-  gameNumber.n++;
-  const myGameNumber = gameNumber.n;
-
-  // console.log(`=== Game ${myGameNumber} ===\n`);
-
   const { player1, player2 } = copyGame(game);
-
-  let round = 0;
 
   while (player1.length > 0 && player2.length > 0) {
     const stringifiedGame = stringifyGame({ player1, player2 });
     if (seen.has(stringifiedGame)) {
-      // console.log("Infinite loop prevention kicking in...");
       return { winner: 1, score: -1 }; // Player 1 wins
     }
     seen.add(stringifiedGame);
-
-    round++;
-    // console.log(`-- Round ${round} (Game ${myGameNumber}) --`);
-    // console.log(`Player 1's deck: ${player1.join(", ")}`);
-    // console.log(`Player 2's deck: ${player2.join(", ")}`);
 
     const card1 = player1.shift();
     const card2 = player2.shift();
     assert(card1 !== undefined);
     assert(card2 !== undefined);
 
-    // console.log(`Player 1 plays: ${card1}`);
-    // console.log(`Player 2 plays: ${card2}`);
-
-    let winner: Player;
-    if (player1.length >= card1 && player2.length >= card2) {
-      // console.log("Playing a sub-game to determine the winner...\n");
-      winner = part2(
-        {
+    const roundWinner: Player =
+      (player1.length >= card1 && player2.length >= card2)
+        ? part2({
           player1: player1.slice(0, card1),
           player2: player2.slice(0, card2),
-        },
-        gameNumber,
-      ).winner;
-      // console.log(`...anyway, back to game ${myGameNumber}.`);
-    } else {
-      winner = card1 > card2 ? 1 : 2;
-    }
+        }).winner
+        : card1 > card2
+        ? 1
+        : 2;
 
-    if (winner === 1) {
+    if (roundWinner === 1) {
       player1.push(card1);
       player1.push(card2);
-    } else if (winner === 2) {
+    } else if (roundWinner === 2) {
       player2.push(card2);
       player2.push(card1);
     }
-
-    // console.log(
-    //   `Player ${winner} wins round ${round} of game ${myGameNumber}!\n`,
-    // );
   }
 
   const winner = player1.length === 0 ? 2 : 1;
   const score = calculateScore([[], player1, player2][winner]);
-
-  // console.log(`The winner of game ${myGameNumber} is player ${winner}!\n`);
 
   return { winner, score };
 };
