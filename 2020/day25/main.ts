5c41e3c13895d0f3bd85b621869ea744beef8276
Bernhard Häussner <bernhard.haeussner@xing.com>
2020-12-25 07:57:17 +0100

    feat(2020/25/TypeScript): solve part 1

---
+++ 2020/day25/main.ts
@@ -0,0 +1,69 @@
+#!/usr/bin/env deno run --allow-read
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import {
+  addCoords,
+  Coord,
+  CoordSet,
+  ensureElementOf,
+  rangeCoords,
+} from "../utils.ts";
+
+const origin = [0, 0] as Coord;
+
+const directions = ["e", "w", "se", "sw", "ne", "nw"] as const;
+type Direction = typeof directions[number];
+
+const parseInput = (string: string): [number, number] => {
+  const numbers = string.trim().split("\n").map(Number);
+  assertEquals(numbers.length, 2);
+  const [a, b] = numbers;
+  return [a, b];
+};
+
+const example = parseInput(`
+  5764801
+  17807724
+`);
+
+assertEquals(example, [5764801, 17807724]);
+
+const input = await Deno.readTextFile("input.txt");
+
+const parsedInput = parseInput(input);
+
+const magicNumber = 20201227;
+
+const figureLoopSize = (transformed: number, base = 7): number => {
+  let loopSize = 0;
+  let result = 1;
+  while (true) {
+    if (result === transformed) return loopSize;
+    loopSize++;
+    result = (base * result) % magicNumber;
+  }
+};
+
+assertEquals(figureLoopSize(5764801), 8);
+assertEquals(figureLoopSize(17807724), 11);
+
+const transform = (base: number, loopSize: number): number => {
+  let result = 1;
+  for (let i = 0; i < loopSize; i++) {
+    result = (base * result) % magicNumber;
+  }
+  return result;
+};
+
+const part1 = (doorAndCardPublicKey: [number, number]): number => {
+  const [doorPublicKey, cardPublicKey] = doorAndCardPublicKey;
+  const doorPrivateKey = figureLoopSize(doorPublicKey);
+  const cardPrivateKey = figureLoopSize(cardPublicKey);
+  const encryptionKeyFromDoor = transform(doorPublicKey, cardPrivateKey);
+  const encryptionKeyFromCard = transform(cardPublicKey, doorPrivateKey);
+  assertEquals(encryptionKeyFromDoor, encryptionKeyFromCard);
+  return encryptionKeyFromDoor;
+};
+
+assertEquals(part1(example), 14897079);
+
+console.log("Result part 1: " + part1(parsedInput));
