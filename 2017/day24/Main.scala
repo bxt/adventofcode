3a46ace834a11a557c785ff97257b8396a1f91db
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-05-03 23:27:20 +0200

    Add 2017/24 part 1 in Scala

---
+++ 2017/day24/Main.scala
@@ -0,0 +1,46 @@
+package day24
+import scala.io.Source
+import scala.collection.mutable.{Map, HashMap}
+
+object Main {
+  case class Component(a: Int, b: Int) {
+    def strength: Int = a + b
+  }
+
+  def strongestBridge(start: Int, components: List[Component]): Int = {
+    if (components.size == 0) {
+      0
+    } else {
+      components
+        .map({ component =>
+          if (component.a == start) {
+            component.strength + strongestBridge(
+              component.b,
+              components.filter { _ != component }
+            )
+          } else if (component.b == start) {
+            component.strength + strongestBridge(
+              component.a,
+              components.filter { _ != component }
+            )
+          } else {
+            0
+          }
+        })
+        .max
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromResource("day24/input.txt").getLines()
+
+    var components = input
+      .map({ line =>
+        val parts = line.split("/")
+        Component(parts(0).toInt, parts(1).toInt)
+      })
+      .toList
+
+    println(strongestBridge(0, components))
+  }
+}
