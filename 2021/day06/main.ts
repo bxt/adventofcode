45c6741f82d7e7036ad59bb002679b0fa5186783
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-06 08:33:18 +0100

    refactor(2021/06): make solution more clear, re-use part 2 for part 1

--- 2021/day06/main.ts
+++ 2021/day06/main.ts
@@ -2,6 +2,9 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 import { sum } from "../../2020/utils.ts";
 
+const BREED_DAYS = 7;
+const MATURE_DAYS = 8;
+
 const parseInput = (string: string): number[] => {
   return string.trim().split(",").map((s) => parseInt(s, 10));
 };
@@ -10,88 +13,79 @@ const text = await Deno.readTextFile("input.txt");
 
 const input = parseInput(text);
 
-const breed = (initialState: number[], iterations: number) => {
-  let state = initialState;
+const singleBreedCount = (daysToBreed: number, daysToGo: number): number =>
+  Math.floor((daysToGo + BREED_DAYS - daysToBreed - 1) / BREED_DAYS);
 
-  for (let i = 0; i < iterations; i++) {
-    state = state.flatMap((s) => s <= 0 ? [6, 8] : [s - 1]);
+assertEquals(singleBreedCount(0, 18), 3);
+assertEquals(singleBreedCount(3, 18), 3);
+assertEquals(singleBreedCount(4, 18), 2);
+assertEquals(singleBreedCount(10, 18), 2);
+assertEquals(singleBreedCount(11, 18), 1);
+assertEquals(singleBreedCount(4, 19), 3);
+assertEquals(singleBreedCount(5, 19), 2);
+assertEquals(singleBreedCount(5, 20), 3);
+assertEquals(singleBreedCount(6, 20), 2);
+assertEquals(singleBreedCount(6, 21), 3);
+
+const childrenDaysToBreed = (
+  daysToBreed: number,
+  daysToGo: number,
+): number[] => {
+  const replicatesTimes = singleBreedCount(daysToBreed, daysToGo);
+  const childNumbers = [];
+  for (let i = 0; i < replicatesTimes; i++) {
+    childNumbers.push(MATURE_DAYS + i * BREED_DAYS + daysToBreed + 1);
   }
-  return state;
+  return childNumbers;
+};
+
+assertEquals(childrenDaysToBreed(3, 18), [12, 19, 26]);
+assertEquals(childrenDaysToBreed(12, 18), [21]);
+assertEquals(childrenDaysToBreed(1, 18), [10, 17, 24]);
+assertEquals(childrenDaysToBreed(10, 18), [19, 26]);
+assertEquals(childrenDaysToBreed(17, 18), [26]);
+[19, 21, 24, 26].forEach((n) => {
+  assertEquals(childrenDaysToBreed(n, 18), []);
+});
+
+const memoize = <R>(func: (n: number) => R): (n: number) => R => {
+  const memory: Record<number, R> = {};
+
+  const memoizedFunc = (n: number): R => {
+    memory[n] ??= func(n);
+    return memory[n];
+  };
+
+  return memoizedFunc;
+};
+
+const unlimitedBreedCount = (
+  daysToBreeds: number[],
+  daysToGo: number,
+): number => {
+  const getTotalSpawns = memoize((daysToBreed: number): number =>
+    1 + sum(
+      childrenDaysToBreed(daysToBreed, daysToGo).map(getTotalSpawns),
+    )
+  );
+
+  return sum(daysToBreeds.map(getTotalSpawns));
 };
 
 const part1 = (input: number[]): number => {
-  return breed(input, 80).length;
+  return unlimitedBreedCount(input, 80);
 };
 
 const example = parseInput(`3,4,3,1,2`);
 
-assertEquals(breed(example, 18).length, 26);
+assertEquals(unlimitedBreedCount(example, 18), 26);
 assertEquals(part1(example), 5934);
 
 console.log("Result part 1: " + part1(input));
 
-const singleBreedCount = (number: number, iterations: number) => {
-  return Math.floor((6 - number + iterations) / 7);
-};
-
-assertEquals(singleBreedCount(0, 18), 3);
-assertEquals(singleBreedCount(1, 18), 3);
-assertEquals(singleBreedCount(2, 18), 3);
-assertEquals(singleBreedCount(3, 18), 3);
-assertEquals(singleBreedCount(4, 18), 2);
-assertEquals(singleBreedCount(5, 18), 2);
-assertEquals(singleBreedCount(6, 18), 2);
-assertEquals(singleBreedCount(7, 18), 2);
-assertEquals(singleBreedCount(8, 18), 2);
-assertEquals(singleBreedCount(9, 18), 2);
-assertEquals(singleBreedCount(10, 18), 2);
-assertEquals(singleBreedCount(11, 18), 1);
-
-assertEquals(singleBreedCount(4, 19), 3);
-assertEquals(singleBreedCount(5, 19), 2);
-assertEquals(singleBreedCount(6, 19), 2);
-
-assertEquals(singleBreedCount(4, 20), 3);
-assertEquals(singleBreedCount(5, 20), 3);
-assertEquals(singleBreedCount(6, 20), 2);
-
-assertEquals(singleBreedCount(4, 21), 3);
-assertEquals(singleBreedCount(5, 21), 3);
-assertEquals(singleBreedCount(6, 21), 3);
-
-const unlimitedBreedCount = (initialState: number[], iterations: number) => {
-  const totalSpawns: Record<number, number> = {};
-
-  const getTotalSpawns = (number: number): number => {
-    if (totalSpawns[number] !== undefined) return totalSpawns[number];
-
-    const selfReplicatesTimes = singleBreedCount(number, iterations);
-    // console.log({ number, selfReplicatesTimes });
-    if (selfReplicatesTimes < 1) return 1;
-
-    const childNumbers = [];
-    for (let i = 0; i < selfReplicatesTimes; i++) {
-      const childNumber = 7 * (i + 1) + number + 2;
-      childNumbers.push(childNumber);
-    }
-    // console.log({ childNumbers });
-    const result = 1 + sum(childNumbers.map(getTotalSpawns));
-    totalSpawns[number] = result;
-    return result;
-  };
-
-  const result = sum(initialState.map(getTotalSpawns));
-  // console.log({ totalSpawns });
-  return result;
-};
-
 const part2 = (input: number[]): number => {
   return unlimitedBreedCount(input, 256);
 };
-
-assertEquals(unlimitedBreedCount(example, 18), 26);
-assertEquals(unlimitedBreedCount(example, 80), 5934);
-assertEquals(unlimitedBreedCount(input, 80), 379414);
 
 assertEquals(part2(example), 26984457539);
 
