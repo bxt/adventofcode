a7b171f02cb4fe4dfda533ced4a5e5acfa08b4e7
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-06 08:49:39 +0100

    refactor(2021/06): replace magic formulas with simple for loop

--- 2021/day06/main.ts
+++ 2021/day06/main.ts
@@ -13,29 +13,20 @@ const text = await Deno.readTextFile("input.txt");
 
 const input = parseInput(text);
 
-const singleBreedCount = (daysToBreed: number, daysToGo: number): number =>
-  Math.floor((daysToGo + BREED_DAYS - daysToBreed - 1) / BREED_DAYS);
-
-assertEquals(singleBreedCount(0, 18), 3);
-assertEquals(singleBreedCount(3, 18), 3);
-assertEquals(singleBreedCount(4, 18), 2);
-assertEquals(singleBreedCount(10, 18), 2);
-assertEquals(singleBreedCount(11, 18), 1);
-assertEquals(singleBreedCount(4, 19), 3);
-assertEquals(singleBreedCount(5, 19), 2);
-assertEquals(singleBreedCount(5, 20), 3);
-assertEquals(singleBreedCount(6, 20), 2);
-assertEquals(singleBreedCount(6, 21), 3);
-
 const childrenDaysToBreed = (
   daysToBreed: number,
   daysToGo: number,
 ): number[] => {
-  const replicatesTimes = singleBreedCount(daysToBreed, daysToGo);
   const childNumbers = [];
-  for (let i = 0; i < replicatesTimes; i++) {
-    childNumbers.push(MATURE_DAYS + i * BREED_DAYS + daysToBreed + 1);
+
+  for (let day = 1; day <= daysToGo; day++) {
+    if (daysToBreed === 0) {
+      daysToBreed = BREED_DAYS;
+      childNumbers.push(day + MATURE_DAYS);
+    }
+    daysToBreed--;
   }
+
   return childNumbers;
 };
 
