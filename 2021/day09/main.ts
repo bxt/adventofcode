79d87c5595ac85298b770e17adb421dc4bda88f5
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-09 06:46:05 +0100

    refactor(2021/09): make use of utils functions

--- 2021/day09/main.ts
+++ 2021/day09/main.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import { Coord, sum } from "../../2020/utils.ts";
+import { Coord, CoordSet, indexWithCoord, sum } from "../../2020/utils.ts";
 
 const parseInput = (string: string): number[][] =>
   string.trim().split("\n").map((line) => {
@@ -17,7 +17,6 @@ const findNeighbors = (input: number[][], [x, y]: Coord) => {
   if (x > 0) neighbors.push([x - 1, y]);
   if (y < input.length - 1) neighbors.push([x, y + 1]);
   if (x < input[y].length - 1) neighbors.push([x + 1, y]);
-
   return neighbors;
 };
 
@@ -25,12 +24,15 @@ const findLowPoints = (input: number[][]): Coord[] => {
   const lowPoints: Coord[] = [];
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
-      const neighbors = findNeighbors(input, [x, y]);
+      const point: Coord = [x, y];
+      const neighbors = findNeighbors(input, point);
 
-      const value = input[y][x];
-      const isLowPoint = neighbors.every(([nx, ny]) => value < input[ny][nx]);
+      const value = indexWithCoord(input, point);
+      const isLowPoint = neighbors.every((n) =>
+        value < indexWithCoord(input, n)
+      );
       if (isLowPoint) {
-        lowPoints.push([x, y]);
+        lowPoints.push(point);
       }
     }
   }
@@ -40,7 +42,7 @@ const findLowPoints = (input: number[][]): Coord[] => {
 const part1 = (input: number[][]): number => {
   const lowPoints = findLowPoints(input);
 
-  const lowPointValues = lowPoints.map(([x, y]) => input[y][x] + 1);
+  const lowPointValues = lowPoints.map((p) => indexWithCoord(input, p) + 1);
 
   return sum(lowPointValues);
 };
@@ -57,29 +59,28 @@ assertEquals(part1(example), 15);
 
 console.log("Result part 1: " + part1(input));
 
+const product = (numbers: number[]) => numbers.reduce((acc, n) => n * acc, 1);
+
 const part2 = (input: number[][]): number => {
   const lowPoints = findLowPoints(input);
 
-  const basins = lowPoints.map((p) => {
-    const basin: Coord[] = [];
-    basin.push(p);
+  const basins = lowPoints.map((lowPoint) => {
+    const basin = new CoordSet();
+    basin.add(lowPoint);
 
-    const addNeighbors = ([x, y]: Coord) => {
-      const neighbors = findNeighbors(input, [x, y]);
+    const addNeighbors = (point: Coord) => {
+      const neighbors = findNeighbors(input, point);
 
-      for (const n of neighbors) {
-        const [nx, ny] = n;
-        const neighborValue = input[ny][nx];
-        if (
-          neighborValue < 9 && !basin.some(([ex, ey]) => ex === nx && ey === ny)
-        ) {
-          basin.push(n);
-          addNeighbors(n);
+      neighbors.forEach((neighbor) => {
+        const neighborValue = indexWithCoord(input, neighbor);
+        if (neighborValue < 9 && !basin.has(neighbor)) {
+          basin.add(neighbor);
+          addNeighbors(neighbor);
         }
-      }
+      });
     };
 
-    addNeighbors(p);
+    addNeighbors(lowPoint);
 
     return basin;
   });
@@ -87,18 +88,16 @@ const part2 = (input: number[][]): number => {
   const map = input.map((l, y) =>
     l.map((v, x) => {
       const ilp = lowPoints.some(([nx, ny]) => nx === x && ny === y);
-      const ibp = !ilp &&
-        basins.some((b) => b.some(([nx, ny]) => nx === x && ny === y));
+      const ibp = !ilp && basins.some((b) => b.has([x, y]));
       return ilp ? "_" : ibp ? "-" : v;
     }).join("")
   ).join("\n");
 
   console.log(map);
 
-  const basinSizes = basins.map((b) => b.length).sort((a, b) => b - a);
+  const basinSizes = basins.map((b) => b.size).sort((a, b) => b - a);
   const largestSizes = basinSizes.slice(0, 3);
-
-  return largestSizes.reduce((acc, n) => n * acc, 1);
+  return product(largestSizes);
 };
 
 assertEquals(part2(example), 1134);
