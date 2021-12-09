41f225686d83aff66ba4d074a082fa74279fbaa5
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-09 06:36:03 +0100

    refactor(2021/09): remove duplicate code

--- 2021/day09/main.ts
+++ 2021/day09/main.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import { Coord, ensureElementOf, sum } from "../../2020/utils.ts";
+import { Coord, sum } from "../../2020/utils.ts";
 
 const parseInput = (string: string): number[][] =>
   string.trim().split("\n").map((line) => {
@@ -11,15 +11,21 @@ const text = await Deno.readTextFile("input.txt");
 
 const input = parseInput(text);
 
-const part1 = (input: number[][]): number => {
+const findNeighbors = (input: number[][], [x, y]: Coord) => {
+  const neighbors: Coord[] = [];
+  if (y > 0) neighbors.push([x, y - 1]);
+  if (x > 0) neighbors.push([x - 1, y]);
+  if (y < input.length - 1) neighbors.push([x, y + 1]);
+  if (x < input[y].length - 1) neighbors.push([x + 1, y]);
+
+  return neighbors;
+};
+
+const findLowPoints = (input: number[][]): Coord[] => {
   const lowPoints: Coord[] = [];
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
-      const neighbors: Coord[] = [];
-      if (y > 0) neighbors.push([x, y - 1]);
-      if (x > 0) neighbors.push([x - 1, y]);
-      if (y < input.length - 1) neighbors.push([x, y + 1]);
-      if (x < input[y].length - 1) neighbors.push([x + 1, y]);
+      const neighbors = findNeighbors(input, [x, y]);
 
       const value = input[y][x];
       const isLowPoint = neighbors.every(([nx, ny]) => value < input[ny][nx]);
@@ -28,14 +34,13 @@ const part1 = (input: number[][]): number => {
       }
     }
   }
-  const lowPointValues = lowPoints.map(([x, y]) => input[y][x] + 1);
+  return lowPoints;
+};
 
-  const map = input.map((l, y) =>
-    l.map((v, x) => {
-      const ilp = lowPoints.some(([nx, ny]) => nx === x && ny === y);
-      return ilp ? "_" : v;
-    }).join("")
-  ).join("\n");
+const part1 = (input: number[][]): number => {
+  const lowPoints = findLowPoints(input);
+
+  const lowPointValues = lowPoints.map(([x, y]) => input[y][x] + 1);
 
   return sum(lowPointValues);
 };
@@ -53,34 +58,14 @@ assertEquals(part1(example), 15);
 console.log("Result part 1: " + part1(input));
 
 const part2 = (input: number[][]): number => {
-  const lowPoints: Coord[] = [];
-  for (let y = 0; y < input.length; y++) {
-    for (let x = 0; x < input[y].length; x++) {
-      const neighbors: Coord[] = [];
-      if (y > 0) neighbors.push([x, y - 1]);
-      if (x > 0) neighbors.push([x - 1, y]);
-      if (y < input.length - 1) neighbors.push([x, y + 1]);
-      if (x < input[y].length - 1) neighbors.push([x + 1, y]);
-
-      const value = input[y][x];
-      const isLowPoint = neighbors.every(([nx, ny]) => value < input[ny][nx]);
-      if (isLowPoint) {
-        lowPoints.push([x, y]);
-      }
-    }
-  }
-  console.log({ lowPoints });
+  const lowPoints = findLowPoints(input);
 
   const basins = lowPoints.map((p) => {
     const basin: Coord[] = [];
     basin.push(p);
 
     const addNeighbors = ([x, y]: Coord) => {
-      const neighbors: Coord[] = [];
-      if (y > 0) neighbors.push([x, y - 1]);
-      if (x > 0) neighbors.push([x - 1, y]);
-      if (y < input.length - 1) neighbors.push([x, y + 1]);
-      if (x < input[y].length - 1) neighbors.push([x + 1, y]);
+      const neighbors = findNeighbors(input, [x, y]);
 
       for (const n of neighbors) {
         const [nx, ny] = n;
@@ -99,8 +84,6 @@ const part2 = (input: number[][]): number => {
     return basin;
   });
 
-  console.log({ basins });
-
   const map = input.map((l, y) =>
     l.map((v, x) => {
       const ilp = lowPoints.some(([nx, ny]) => nx === x && ny === y);
@@ -114,7 +97,6 @@ const part2 = (input: number[][]): number => {
 
   const basinSizes = basins.map((b) => b.length).sort((a, b) => b - a);
   const largestSizes = basinSizes.slice(0, 3);
-  console.log({ basinSizes, largestSizes });
 
   return largestSizes.reduce((acc, n) => n * acc, 1);
 };
