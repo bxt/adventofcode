8f2a5cafc93d872b705c4c790665d6326a864e9f
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-09 01:38:28 +0100

    feat(2016/19): solve part 2 with PHP but its super slow

--- 2016/day19/main.php
+++ 2016/day19/main.php
@@ -34,4 +34,37 @@ echo "Part 1: $result\n";
 
 
 
+$next_elves = [];
+
+$next_elves[$input] = 1;
+for ($i = 1; $i < $input; $i++) {
+  $next_elves[$i] = $i + 1;
+}
+
+$elves_with_gifts = $input;
+
+$current_elf = 1;
+
+while ($elves_with_gifts > 1) {
+  $one_before_steal_from = $current_elf;
+  $steps_to_take = floor($elves_with_gifts / 2.0);
+  for ($i = 1; $i < $steps_to_take; $i++) {
+    $one_before_steal_from = $next_elves[$one_before_steal_from];
+  }
+  $steal_from = $next_elves[$one_before_steal_from];
+  $next_elves[$one_before_steal_from] = $next_elves[$steal_from];
+  // echo "Elf $current_elf steals everything from $steal_from! ($steps_to_take / $elves_with_gifts)\n";
+  $current_elf = $next_elves[$current_elf];
+  $elves_with_gifts--;
+
+  if($elves_with_gifts % 1000 == 0) {
+    echo "$elves_with_gifts elves with gifts...\n";
+  }
+}
+
+$result = $current_elf;
+
+echo "Part 2: $result\n";
+
+
 
