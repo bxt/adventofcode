5886ae7c0e9ebec19652818a814102edb4d6a530
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-09 06:28:13 +0100

    feat(2021/09): solve part 2

--- 2021/day09/main.ts
+++ 2021/day09/main.ts
@@ -21,8 +21,6 @@ const part1 = (input: number[][]): number => {
       if (y < input.length - 1) neighbors.push([x, y + 1]);
       if (x < input[y].length - 1) neighbors.push([x + 1, y]);
 
-      console.log({ x, y, neighbors });
-
       const value = input[y][x];
       const isLowPoint = neighbors.every(([nx, ny]) => value < input[ny][nx]);
       if (isLowPoint) {
@@ -30,8 +28,6 @@ const part1 = (input: number[][]): number => {
       }
     }
   }
-  console.log({ lowPoints });
-
   const lowPointValues = lowPoints.map(([x, y]) => input[y][x] + 1);
 
   const map = input.map((l, y) =>
@@ -40,8 +36,6 @@ const part1 = (input: number[][]): number => {
       return ilp ? "_" : v;
     }).join("")
   ).join("\n");
-
-  console.log(map);
 
   return sum(lowPointValues);
 };
@@ -57,3 +51,74 @@ const example = parseInput(`
 assertEquals(part1(example), 15);
 
 console.log("Result part 1: " + part1(input));
+
+const part2 = (input: number[][]): number => {
+  const lowPoints: Coord[] = [];
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const neighbors: Coord[] = [];
+      if (y > 0) neighbors.push([x, y - 1]);
+      if (x > 0) neighbors.push([x - 1, y]);
+      if (y < input.length - 1) neighbors.push([x, y + 1]);
+      if (x < input[y].length - 1) neighbors.push([x + 1, y]);
+
+      const value = input[y][x];
+      const isLowPoint = neighbors.every(([nx, ny]) => value < input[ny][nx]);
+      if (isLowPoint) {
+        lowPoints.push([x, y]);
+      }
+    }
+  }
+  console.log({ lowPoints });
+
+  const basins = lowPoints.map((p) => {
+    const basin: Coord[] = [];
+    basin.push(p);
+
+    const addNeighbors = ([x, y]: Coord) => {
+      const neighbors: Coord[] = [];
+      if (y > 0) neighbors.push([x, y - 1]);
+      if (x > 0) neighbors.push([x - 1, y]);
+      if (y < input.length - 1) neighbors.push([x, y + 1]);
+      if (x < input[y].length - 1) neighbors.push([x + 1, y]);
+
+      for (const n of neighbors) {
+        const [nx, ny] = n;
+        const neighborValue = input[ny][nx];
+        if (
+          neighborValue < 9 && !basin.some(([ex, ey]) => ex === nx && ey === ny)
+        ) {
+          basin.push(n);
+          addNeighbors(n);
+        }
+      }
+    };
+
+    addNeighbors(p);
+
+    return basin;
+  });
+
+  console.log({ basins });
+
+  const map = input.map((l, y) =>
+    l.map((v, x) => {
+      const ilp = lowPoints.some(([nx, ny]) => nx === x && ny === y);
+      const ibp = !ilp &&
+        basins.some((b) => b.some(([nx, ny]) => nx === x && ny === y));
+      return ilp ? "_" : ibp ? "-" : v;
+    }).join("")
+  ).join("\n");
+
+  console.log(map);
+
+  const basinSizes = basins.map((b) => b.length).sort((a, b) => b - a);
+  const largestSizes = basinSizes.slice(0, 3);
+  console.log({ basinSizes, largestSizes });
+
+  return largestSizes.reduce((acc, n) => n * acc, 1);
+};
+
+assertEquals(part2(example), 1134);
+
+console.log("Result part 2: " + part2(input));
