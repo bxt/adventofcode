5adce9d2467321d4df409c4a7b44f7a751111b02
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-10 12:10:06 +0100

    refactor(2021/10): clean up

--- 2021/day10/main.ts
+++ 2021/day10/main.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import {
-  Coord,
-  CoordSet,
-  indexWithCoord,
-  product,
-  sum,
-} from "../../2020/utils.ts";
+import { sum } from "../../2020/utils.ts";
 
 const scores = { ")": 3, "]": 57, "}": 1197, ">": 25137 };
-const matchingBraces = { ")": "(", "]": "[", "}": "{", ">": "<" };
 type ClosingBrace = keyof typeof scores;
 
 const completionScores = { "(": 1, "[": 2, "{": 3, "<": 4 };
 type OpeningBrace = keyof typeof completionScores;
+
+const matchingBraces: Record<ClosingBrace, OpeningBrace> = {
+  ")": "(",
+  "]": "[",
+  "}": "{",
+  ">": "<",
+};
 
 const parseInput = (string: string): string[][] =>
   string.trim().split("\n").map((line) => {
@@ -30,21 +30,27 @@ const isClosingBrace = (character: string): character is ClosingBrace =>
 const isOpeningBrace = (character: string): character is OpeningBrace =>
   Object.keys(completionScores).includes(character);
 
-const checkLine = (line: string[]) => {
-  const stack = [];
+type UnexpectedResult = { unexpected: ClosingBrace };
+type UnmatchedResult = { unmatched: OpeningBrace[] };
+type OkayResult = { okay: true };
+type Result = UnexpectedResult | UnmatchedResult | OkayResult;
+
+const isUnexpectedResult = (result: Result): result is UnexpectedResult =>
+  "unexpected" in result;
+const isUnmatchedResult = (result: Result): result is UnmatchedResult =>
+  "unmatched" in result;
+
+const checkLine = (line: string[]): Result => {
+  const stack: OpeningBrace[] = [];
   for (const character of line) {
     if (isClosingBrace(character)) {
-      const tip = stack.pop();
-      if (matchingBraces[character] !== tip) {
-        console.log({
-          expected: matchingBraces[character],
-          found: tip,
-          line,
-        });
+      if (matchingBraces[character] !== stack.pop()) {
         return { unexpected: character };
       }
-    } else {
+    } else if (isOpeningBrace(character)) {
       stack.push(character);
+    } else {
+      // ignore everything other than braces
     }
   }
   if (stack.length > 0) {
@@ -55,11 +61,11 @@ const checkLine = (line: string[]) => {
 };
 
 const part1 = (input: string[][]): number => {
-  return sum(input.map((line) => {
-    const result = checkLine(line);
-    if (result.unexpected) return scores[result.unexpected];
-    return 0;
-  }));
+  return sum(
+    input.map(checkLine).filter(isUnexpectedResult).map((result) =>
+      scores[result.unexpected]
+    ),
+  );
 };
 
 const example = parseInput(`
@@ -79,12 +85,6 @@ assertEquals(part1(example), 26397);
 
 console.log("Result part 1: " + part1(input));
 
-export const largestBasinRating = (basins: CoordSet[]) => {
-  const basinSizes = basins.map((b) => b.size).sort((a, b) => b - a);
-  const largestSizes = basinSizes.slice(0, 3);
-  return product(largestSizes);
-};
-
 const median = (values: number[]) =>
   [...values].sort((a, b) => b - a)[Math.floor((values.length - 1) / 2)];
 
@@ -93,21 +93,14 @@ assertEquals(median([6, 4, 5]), 5);
 assertEquals(median([288957, 5566, 1480781, 995444, 294]), 288957);
 
 const part2 = (input: string[][]): number => {
-  const socres = input.map((line) => {
-    const result = checkLine(line);
-
-    if (!result.unmatched) return undefined;
-
-    return result.unmatched.reduceRight((acc, character) => {
-      if (!isOpeningBrace(character)) throw new Error(character);
-      const x = completionScores[character];
-      console.log({ character, x, acc });
-      return acc * 5 + x;
-    }, 0);
-  }).filter((x): x is number => x !== undefined);
-  console.log({ socres });
-
-  return median(socres);
+  return median(
+    input.map(checkLine).filter(isUnmatchedResult).map((result) =>
+      result.unmatched.reduceRight(
+        (acc, character) => acc * 5 + completionScores[character],
+        0,
+      )
+    ),
+  );
 };
 
 assertEquals(part2(example), 288957);
