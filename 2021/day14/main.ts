f20128a5c20ee3ef38efaf71d915a4e1f9071c14
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-14 06:51:21 +0100

    feat(2021/14): solve part 2

--- 2021/day14/main.ts
+++ 2021/day14/main.ts
@@ -61,6 +61,9 @@ const part1 = (input: Input): number => {
     counts[left] ??= 0;
     counts[left]++;
   }
+
+  console.log({ counts });
+
   const [min, max] = minMax(Object.entries(counts).map(([, count]) => count));
 
   return max - min;
@@ -90,3 +93,82 @@ const example = parseInput(`
 assertEquals(part1(example), 1588);
 
 console.log("Result part 1: " + part1(input));
+
+const mergeCounts = (
+  counts1: Record<string, number>,
+  counts2: Record<string, number>,
+) => {
+  const counts: Record<string, number> = {};
+  Object.entries(counts1).forEach(([key, count]) => {
+    counts[key] = count;
+  });
+  Object.entries(counts2).forEach(([key, count]) => {
+    counts[key] ??= 0;
+    counts[key] += count;
+  });
+  return counts;
+};
+
+type Counts = Record<string, number>;
+
+const addCounts = (
+  into: Counts,
+  counts: Counts,
+) => {
+  Object.entries(counts).forEach(([key, count]) => {
+    into[key] ??= 0;
+    into[key] += count;
+  });
+};
+
+const part2 = (input: Input): number => {
+  const elementCounts: Record<number, Record<string, Counts>> = {};
+  const afterStep = 40;
+
+  const result = input.template;
+  const counts: Counts = {};
+
+  const getCounts = (pair: string, iterationsLeft: number): Counts => {
+    console.log({ pair, iterationsLeft });
+    const counts: Counts = {};
+    if (elementCounts[iterationsLeft]?.[pair]) {
+      return elementCounts[iterationsLeft][pair];
+    }
+    const [left, right] = pair.split("");
+    const insertion = input.rules[`${left}${right}`];
+    if (insertion) {
+      addCounts(counts, { [insertion]: 1 });
+      if (iterationsLeft > 0) {
+        addCounts(counts, getCounts(`${left}${insertion}`, iterationsLeft - 1));
+        addCounts(
+          counts,
+          getCounts(`${insertion}${right}`, iterationsLeft - 1),
+        );
+      }
+    }
+    elementCounts[iterationsLeft] ??= {};
+    elementCounts[iterationsLeft][pair] = counts;
+    return counts;
+  };
+
+  for (let j = 0; j < result.length; j++) {
+    const left = result.charAt(j);
+    addCounts(counts, { [left]: 1 });
+    if (j < result.length - 1) {
+      const right = result.charAt(j + 1);
+      addCounts(counts, getCounts(`${left}${right}`, afterStep - 1));
+    }
+  }
+
+  console.log({ counts });
+
+  const [min, max] = minMax(Object.entries(counts).map(([, count]) => count));
+
+  console.log({ min, max });
+
+  return max - min;
+};
+
+assertEquals(part2(example), 2188189693529);
+
+console.log("Result part 2: " + part2(input));
