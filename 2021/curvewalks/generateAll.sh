606d9e5fcbca827d289ec99eb26eb0edb2c22a49
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-12-16 01:45:41 +0100

    feat(2021): add script to generate a bunch of curve examples

---
+++ 2021/curvewalks/generateAll.sh
@@ -0,0 +1,20 @@
+./bin.ts --curve gosper --recursion 2 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve gosper --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve gosper --recursion 3 --height 400 --width 400 --margin 20 --drawLine
+./bin.ts --curve gosper --recursion 3 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve gosper --recursion 3 --height 200 --width 200 --margin 20 --dotFraction 2.5
+./bin.ts --curve boxy --recursion 1 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve boxy --recursion 2 --height 200 --width 200 --margin 20
+./bin.ts --curve peano --recursion 2 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve peano --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve hilbert --recursion 2 --height 200 --width 200 --margin 40 --drawLine
+./bin.ts --curve hilbert --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve hilbert --recursion 4 --height 200 --width 200 --margin 20
+./bin.ts --curve kochSnowflake --recursion 2 --height 200 --width 200 --margin 20
+./bin.ts --curve kochSnowflake --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve sierpinski --recursion 2 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve sierpinski --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve sierpinskiArrowhead --recursion 2 --height 200 --width 200 --margin 20 --drawLine
+./bin.ts --curve sierpinskiArrowhead --recursion 3 --height 200 --width 200 --margin 20
+./bin.ts --curve sierpinskiArrowhead --recursion 4 --height 200 --width 200 --margin 20
+./bin.ts --curve sierpinskiArrowhead --recursion 5 --height 400 --width 400 --margin 20 --drawLine
