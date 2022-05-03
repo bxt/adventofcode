34652eb6583067ea27b309e4c2e4a69cfde6d1ec
Bernhard Häussner <bxt@die-optimisten.net>
2022-05-04 00:34:04 +0200

    feat(2016/20): finish Rust implementation

--- 2016/day20/main.rs
+++ 2016/day20/main.rs
@@ -1,30 +1,45 @@
-use std::fs::File;
-use std::io;
+use std::cmp;
 
-// A     -------
-// D --
-// -> do nothing
-//
-// A     -------
-// D    ----------
-// -> drop
-//
-// A     -------
-// D ------
-// -> move front
-//
-// A     -------
-// D       --
-// -> move back, create second part
-//
-// A     -------
-// D         ------
-// -> move back
-//
-// A     -------
-// D              --
-// -> do nothing
-//
+fn parse_line(line: &str) -> Result<(u32, u32), Box<dyn std::error::Error>> {
+    let mut pieces = line.split("-");
+    let from_string = pieces.next().ok_or("no from")?;
+    let to_string = pieces.next().ok_or("no to")?;
+    let disallow_from: u32 = from_string.parse()?;
+    let disallow_to: u32 = to_string.parse()?;
+    Ok((disallow_from, disallow_to))
+}
+
+fn disallow(allowed: Vec<(u32, u32)>, (disallow_from, disallow_to): (u32, u32)) -> Vec<(u32, u32)> {
+    let mut new_allowed = vec![];
+
+    for (allowed_from, allowed_to) in allowed {
+        if disallow_from <= allowed_from {
+            if disallow_to < allowed_to {
+                new_allowed.push((cmp::max(disallow_to + 1, allowed_from), allowed_to))
+            }
+        } else {
+            new_allowed.push((allowed_from, cmp::min(disallow_from - 1, allowed_to)));
+            if disallow_to < allowed_to {
+                new_allowed.push((disallow_to + 1, allowed_to));
+            }
+        }
+    }
+
+    new_allowed
+}
+
+#[test]
+fn chec_disallow() {
+    assert_eq!(disallow(vec![(5, 7)], (2, 4)), vec![(5, 7)]);
+    assert_eq!(disallow(vec![(5, 7)], (5, 7)), vec![]);
+    assert_eq!(disallow(vec![(5, 7)], (2, 7)), vec![]);
+    assert_eq!(disallow(vec![(5, 7)], (2, 6)), vec![(7, 7)]);
+    assert_eq!(disallow(vec![(5, 7)], (5, 5)), vec![(6, 7)]);
+    assert_eq!(disallow(vec![(5, 7)], (6, 6)), vec![(5, 5), (7, 7)]);
+    assert_eq!(disallow(vec![(5, 7)], (6, 8)), vec![(5, 5)]);
+    assert_eq!(disallow(vec![(5, 7)], (7, 8)), vec![(5, 6)]);
+    assert_eq!(disallow(vec![(5, 7)], (8, 9)), vec![(5, 7)]);
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::read_to_string("input.txt")?;
@@ -32,54 +47,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut allowed = vec![(0u32, u32::MAX)];
 
     for line in file.lines() {
-        let mut pieces = line.split("-");
-        let from_string = pieces.next().ok_or("no from")?;
-        let to_string = pieces.next().ok_or("no to")?;
-        let disallow_from: u32 = from_string.parse()?;
-        let disallow_to: u32 = to_string.parse()?;
-        println!("{} to {}", disallow_from, disallow_to);
-
-        if disallow_from == 0 {
-            while allowed[0].1 <= disallow_to {
-                allowed.remove(0);
-            }
-            if allowed[0].0 <= disallow_to {
-                allowed[0].0 = disallow_to + 1;
-            }
-            println!("-----666---- {:?}", allowed[0]);
-        } else {
-            let mut new_allowed = vec![];
-
-            for pair in allowed.iter_mut() {
-                let (allowed_from, allowed_to) = pair;
-                println!(
-                    "-----1---- {} {} {} {} {} {}",
-                    *allowed_from,
-                    disallow_from,
-                    *allowed_to,
-                    disallow_from,
-                    allowed_from,
-                    allowed_to
-                );
-
-                if disallow_from >= *allowed_from && disallow_from <= *allowed_to {
-                    if *allowed_to >= disallow_to {
-                        println!("-----3----");
-                        new_allowed.push((disallow_to + 1, *allowed_to))
-                    }
-
-                    println!("-----2----");
-                    *allowed_to = disallow_from - 1;
-                }
-            }
-
-            println!("append {:?}", new_allowed);
-
-            allowed.append(&mut new_allowed);
-        }
-
-        println!("{:?}", allowed);
+        let disallow_range = parse_line(line)?;
+        allowed = disallow(allowed, disallow_range);
     }
+
+    println!("part 1: {}", allowed[0].0);
+
+    let mut sum = 0u32;
+
+    for (allowed_from, allowed_to) in allowed {
+        sum += allowed_to - allowed_from + 1;
+    }
+
+    println!("part 2: {}", sum);
 
     Ok(())
 }
