52c7f306160f0c9d1bb747181b52aee2e588e6d9
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-12-01 06:11:52 +0100

    wip

--- 2021/day24/main.ts
+++ 2021/day24/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env deno run --allow-read
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 
-const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5];
+const inputs = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 9, 9];
 
 const offsets = [13, 12, 10, -11, 14, 13, 12, -5, 10, 0, -11, -13, -13, -11];
 
@@ -12,22 +12,17 @@ let z = 0;
 for (let i = 0; i < 14; i++) {
   const input = inputs[i];
 
-  const checkW = z % 26 + offsets[i] !== input;
-  // ^^^ can also only ever be true if (offsets[i] <= 0)
+  const equals = z % 26 + offsets[i] === input;
 
   const divZ = offsets[i] <= 0 ? 26 : 1;
   z = Math.floor(z / divZ);
 
-  const factor = (checkW ? 1 : 26);
-  z *= factor;
-
-  if (checkW) {
-    const summand = input + summands[i];
-
-    z += summand;
+  if (equals) {
+    z *= 26;
+    z += input + summands[i];
   }
 
-  console.log({ input, checkW, factor, z });
+  console.log({ input, equals, z });
 }
 
 assertEquals(z, 0);
