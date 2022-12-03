e0e6f80fb32b4162ebc3fee3ea49cff77005cc2b
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-12-03 07:04:08 +0100

    feat(2022/03): solve part 2 with rust

--- 2022/day03/main.rs
+++ 2022/day03/main.rs
@@ -1,36 +1,34 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
-fn parse_input(input: &str) -> Vec<(&str, &str)> {
+fn parse_input(input: &str) -> Vec<&str> {
     input.trim()
     .split("\n")
-    .flat_map(|line| parse_line(line)).collect::<Vec<(&str, &str)>>()
+    .map(|line| line.trim()).collect::<Vec<&str>>()
 }
 
-fn parse_line(line: &str) -> Result<(&str, &str), Box<dyn std::error::Error>> {
-    let trimmed_line = line.trim();
-    let pieces = line.trim().split_at(trimmed_line.len()/2);
-    Ok(pieces)
+fn get_priority(item: &u8) -> u32 {
+    match item {
+        b'a' ..= b'z' => {
+            Into::<u32>::into(item - b'a' + 1)
+        }
+        b'A'..= b'Z' => {
+            Into::<u32>::into(item - b'A' + 27)
+        }
+        _ => { panic!("oy2!") }
+    }
 }
 
-fn part1(rucksacks: &Vec<(&str, &str)>) -> u32 {
-    rucksacks.iter().map(|(first, last)| {
+fn part1(rucksacks: &Vec<&str>) -> u32 {
+    rucksacks.iter().map(|rucksack| {
+        let (first, last) = rucksack.trim().split_at(rucksack.len()/2);
         let first_set: HashSet<u8> = HashSet::from_iter(first.bytes());
         let last_set: HashSet<u8> = HashSet::from_iter(last.bytes());
         let common = first_set.intersection(&last_set);
         let mut common_iterator = common.into_iter();
         let common_byte = common_iterator.next().unwrap();
         if common_iterator.next() != None {panic!("oy!")}
-        let score = match common_byte {
-            b'a' ..= b'z' => {
-                Into::<u32>::into(common_byte - b'a' + 1)
-            }
-            b'A'..= b'Z' => {
-                Into::<u32>::into(common_byte - b'A' + 27)
-            }
-            _ => { panic!("oy2!") }
-        };
-        score
+        get_priority(common_byte)
     }).sum::<u32>()
 }
 
@@ -40,6 +38,32 @@ fn check_part1() {
     assert_eq!(part1(&parse_input(example)), 157);
 }
 
+fn part2(rucksacks: &Vec<&str>) -> u32 {
+    rucksacks.chunks(3).map(|chunk| {
+        match chunk {
+            [rucksack1, rucksack2, rucksack3] => {
+                let set1: HashSet<u8> = HashSet::from_iter(rucksack1.bytes());
+                let set2: HashSet<u8> = HashSet::from_iter(rucksack2.bytes());
+                let set3: HashSet<u8> = HashSet::from_iter(rucksack3.bytes());
+
+                let mut common = set1;
+                common.retain(|byte| set2.contains(byte) && set3.contains(byte));
+                let mut common_iterator = common.into_iter();
+                let common_byte = common_iterator.next().unwrap();
+                if common_iterator.next() != None {panic!("oy!")}
+                get_priority(&common_byte)
+            }
+            _ => { panic!("oy3!") }
+        }
+    }).sum::<u32>()
+}
+
+#[test]
+fn check_part2() {
+    let example = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+    assert_eq!(part2(&parse_input(example)), 70);
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::read_to_string("day03/input.txt")?;
 
@@ -47,6 +71,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let part1 = part1(&parsed_input);
     println!("part 1: {}", part1);
+
+
+    let part2 = part2(&parsed_input);
+    println!("part 2: {}", part2);
 
     Ok(())
 }
