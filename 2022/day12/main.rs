5788742c7e59fda0944fe8de35b8ae6245bbc2fb
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-12-12 08:04:51 +0100

    feat(2022/12): separate part 2 properly and clean up

--- 2022/day12/main.rs
+++ 2022/day12/main.rs
@@ -19,7 +19,7 @@ impl HeightMap {
         self.items[y][x]
     }
 
-    fn neighbours(&self, around: Position) -> Vec<Position> {
+    fn potential_neighbours(&self, around: Position) -> Vec<Position> {
         let (x, y) = around;
         let mut potential_neighbours = vec![];
         if x > 0 {
@@ -35,12 +35,11 @@ impl HeightMap {
             potential_neighbours.push((x, y + 1));
         }
 
-        let around_value = self.get(around);
-
         potential_neighbours
-            .into_iter()
-            .filter(|&pos| around_value <= self.get(pos) + 1)
-            .collect()
+    }
+
+    fn can_go(&self, from: Position, to: Position) -> bool {
+        self.get(to) <= self.get(from) + 1
     }
 }
 
@@ -58,11 +57,11 @@ impl FromStr for HeightMap {
                 match ch {
                     b'S' => {
                         line_items.push(0);
-                        end_position = Some((x, y))
+                        start_position = Some((x, y))
                     }
                     b'E' => {
                         line_items.push(b'z' - b'a');
-                        start_position = Some((x, y))
+                        end_position = Some((x, y))
                     }
                     b'a'..=b'z' => line_items.push(ch - b'a'),
                     _ => panic!("Oy? {ch}"),
@@ -85,20 +84,58 @@ fn parse_input(input: &str) -> HeightMap {
     input.parse().unwrap()
 }
 
-fn part1(input: &HeightMap) -> u64 {
-    // println!("{input:?}");
+trait DijkstraInput {
+    fn neighbours(&self, around: Position) -> Vec<Position>;
+    fn get_start(&self) -> Position;
+    fn is_target(&self, potential_target: Position) -> bool;
+}
 
-    // let mut best_distances = RefCell::new(HashMap::new());
+impl DijkstraInput for HeightMap {
+    fn neighbours(&self, around: Position) -> Vec<Position> {
+        self.potential_neighbours(around)
+            .into_iter()
+            .filter(|&to| self.can_go(around, to))
+            .collect()
+    }
+
+    fn get_start(&self) -> Position {
+        self.start_position
+    }
+
+    fn is_target(&self, potential_target: Position) -> bool {
+        potential_target == self.end_position
+    }
+}
+
+struct ReversedHeightMap<'a>(&'a HeightMap);
+
+impl DijkstraInput for ReversedHeightMap<'_> {
+    fn neighbours(&self, around: Position) -> Vec<Position> {
+        self.0
+            .potential_neighbours(around)
+            .into_iter()
+            .filter(|&to| self.0.can_go(to, around))
+            .collect()
+    }
+
+    fn get_start(&self) -> Position {
+        self.0.end_position
+    }
+
+    fn is_target(&self, potential_target: Position) -> bool {
+        self.0.get(potential_target) == 0
+    }
+}
+
+fn dijkstra(input: &impl DijkstraInput) -> u64 {
     let mut best_distances = HashMap::new();
     let mut known_distances = HashMap::new();
     let mut queue = VecDeque::new();
 
-    best_distances.insert(input.start_position, 0u64);
-    queue.push_back(input.start_position);
+    best_distances.insert(input.get_start(), 0u64);
+    queue.push_back(input.get_start());
 
     while queue.len() > 0 {
-        // println!("queue: {queue:?}");
-
         let current = *queue
             .iter()
             .min_by(|a, b| {
@@ -110,28 +147,19 @@ fn part1(input: &HeightMap) -> u64 {
             .unwrap();
         queue.retain(|&e| e != current);
 
-        // println!("current: {current:?}");
-        // println!("best_distances: {best_distances:?}");
-        // println!("known_distances: {known_distances:?}");
-        // println!("-------");
-
         let current_distance = *best_distances.get(&current).unwrap();
         known_distances.insert(current, current_distance);
 
-        if input.get(current) == 0 {
-            // println!("FFFFFOUND! {} for {:?}", input.get(current), current);
-
+        if input.is_target(current) {
             return current_distance;
         }
 
         for neighbour in input.neighbours(current) {
-            // println!("    neighbour: {neighbour:?}");
-
             if !known_distances.contains_key(&neighbour) {
                 let possible_distance = best_distances.get(&current).unwrap() + 1;
                 if best_distances
                     .get(&neighbour)
-                    .map(|&d| possible_distance < d)
+                    .map(|&best_distance| possible_distance < best_distance)
                     .unwrap_or(true)
                 {
                     best_distances.insert(neighbour, possible_distance);
@@ -141,13 +169,31 @@ fn part1(input: &HeightMap) -> u64 {
         }
     }
 
-    unreachable!();
+    unreachable!()
+}
+
+fn part1(input: &HeightMap) -> u64 {
+    dijkstra(input)
 }
 
 #[test]
 fn check_part1() {
     assert_eq!(
         part1(&parse_input(
+            &std::fs::read_to_string("day12/example.txt").unwrap()
+        )),
+        31
+    );
+}
+
+fn part2(input: &HeightMap) -> u64 {
+    dijkstra(&ReversedHeightMap(input))
+}
+
+#[test]
+fn check_part2() {
+    assert_eq!(
+        part2(&parse_input(
             &std::fs::read_to_string("day12/example.txt").unwrap()
         )),
         29
@@ -162,8 +208,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let part1 = part1(&parsed_input);
     println!("part 1: {}", part1);
 
-    // let part2 = part2(&parsed_input);
-    // println!("part 2: {}", part2);
+    let part2 = part2(&parsed_input);
+    println!("part 2: {}", part2);
 
     Ok(())
 }
