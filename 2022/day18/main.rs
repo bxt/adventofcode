4e8be76923db09f1c32222cf3f35ddf01390455f
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-12-18 22:57:14 +0100

    feat(2022/18): solve part 2 in rust

--- 2022/day18/main.rs
+++ 2022/day18/main.rs
@@ -1,4 +1,19 @@
-fn parse_input(input: &str) -> Vec<(i32, i32, i32)> {
+use std::collections::HashSet;
+
+type Point = (i32, i32, i32);
+
+fn neighbors(&(x, y, z): &Point) -> [Point; 6] {
+    [
+        (x + 1, y, z),
+        (x - 1, y, z),
+        (x, y + 1, z),
+        (x, y - 1, z),
+        (x, y, z + 1),
+        (x, y, z - 1),
+    ]
+}
+
+fn parse_input(input: &str) -> Vec<Point> {
     input
         .trim()
         .lines()
@@ -14,25 +29,20 @@ fn parse_input(input: &str) -> Vec<(i32, i32, i32)> {
         .collect()
 }
 
-fn part1(input: &Vec<(i32, i32, i32)>) -> usize {
-    input.len();
-
+fn count_uncovered_sides(input: &Vec<Point>) -> usize {
     input
         .iter()
-        .map(|&(x, y, z)| {
-            [
-                (x + 1, y, z),
-                (x - 1, y, z),
-                (x, y + 1, z),
-                (x, y - 1, z),
-                (x, y, z + 1),
-                (x, y, z - 1),
-            ]
-            .into_iter()
-            .filter(|c| !input.contains(c))
-            .count()
+        .map(|p| {
+            neighbors(p)
+                .into_iter()
+                .filter(|c| !input.contains(c))
+                .count()
         })
         .sum()
+}
+
+fn part1(input: &Vec<Point>) -> usize {
+    count_uncovered_sides(input)
 }
 
 #[test]
@@ -46,6 +56,46 @@ fn check_part1() {
     );
 }
 
+fn part2(input: &Vec<Point>) -> usize {
+    let mut airs: HashSet<Point> = HashSet::new();
+    let rocks: HashSet<&Point> = HashSet::from_iter(input.iter());
+
+    let min_x = input.iter().map(|p| p.0).min().unwrap() - 1;
+    let max_x = input.iter().map(|p| p.0).max().unwrap() + 1;
+    let min_y = input.iter().map(|p| p.1).min().unwrap() - 1;
+    let max_y = input.iter().map(|p| p.1).max().unwrap() + 1;
+    let min_z = input.iter().map(|p| p.2).min().unwrap() - 1;
+    let max_z = input.iter().map(|p| p.2).max().unwrap() + 1;
+
+    let mut todo = vec![(min_x, min_y, min_z)];
+
+    while let Some(p) = todo.pop() {
+        if !airs.contains(&p) && !rocks.contains(&p) {
+            let (x, y, z) = p;
+            if x >= min_x && x <= max_x && y >= min_y && y <= max_y && z >= min_z && z <= max_z {
+                airs.insert(p);
+                todo.extend(neighbors(&p).iter());
+            }
+        }
+    }
+
+    let outsides = (1 + max_x - min_x) * (1 + max_y - min_y)
+        + (1 + max_x - min_x) * (1 + max_z - min_z)
+        + (1 + max_y - min_y) * (1 + max_z - min_z);
+    let uncovered = count_uncovered_sides(&airs.into_iter().collect());
+    uncovered - 2 * usize::try_from(outsides).unwrap()
+}
+
+#[test]
+fn check_part2() {
+    assert_eq!(
+        part2(&parse_input(
+            &std::fs::read_to_string("day18/example.txt").unwrap()
+        )),
+        58
+    );
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::read_to_string("day18/input.txt")?;
 
@@ -54,8 +104,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let part1 = part1(&parsed_input);
     println!("part 1: {}", part1);
 
-    // let part2 = part2(&parsed_input);
-    // println!("part 2: {}", part2);
+    let part2 = part2(&parsed_input);
+    println!("part 2: {}", part2);
 
     Ok(())
 }
