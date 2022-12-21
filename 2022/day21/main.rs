ed3e8d62a7329e5668b0cb5848e0c3184969ed07
Bernhard Häussner <bxt@die-optimisten.net>
2022-12-21 16:49:39 +0100

    feat(2022/21): solve part 2 with rust
    Again just pasted from replit, need to make nice later

--- 2022/day21/main.rs
+++ 2022/day21/main.rs
@@ -42,3 +42,91 @@ fn main() {
     }
     dbg!(known.get("root"));
 }
+
+// p2:
+use std::collections::HashMap;
+
+fn main() {
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    println!("Hello, world!");
+    let mut known = HashMap::new();
+    let mut unknown = vec![];
+    for line in input.trim().lines() {
+        let (var, value) = line.split_once(": ").unwrap();
+        if let Ok(number) = value.parse::<i128>() {
+            known.insert(var, number);
+        } else {
+            let (fst, op_and_snd) = value.split_once(" ").unwrap();
+            let (op, snd) = op_and_snd.split_once(" ").unwrap();
+            unknown.push((var, fst, op, snd));
+        }
+    }
+    dbg!(known.len());
+    known.remove("humn");
+    dbg!(known.len());
+    while !known.contains_key("humn") {
+        unknown = unknown
+            .into_iter()
+            .filter(|(var, fst, op, snd)| {
+                if var == &"root" {
+                    if known.contains_key(fst) || known.contains_key(snd) {
+                        if known.contains_key(fst) {
+                            let fst_val = known.get(fst).unwrap();
+                            known.insert(snd, *fst_val);
+                        } else {
+                            let snd_val = known.get(snd).unwrap();
+                            known.insert(fst, *snd_val);
+                        }
+                        return false;
+                    }
+                } else if known.contains_key(fst) && known.contains_key(snd) {
+                    let fst_val = known.get(fst).unwrap();
+                    let snd_val = known.get(snd).unwrap();
+                    known.insert(
+                        var,
+                        match op {
+                            &"+" => fst_val + snd_val,
+                            &"-" => fst_val - snd_val,
+                            &"*" => fst_val * snd_val,
+                            &"/" => fst_val / snd_val,
+                            _ => panic!("unknown op: {op}"),
+                        },
+                    );
+                    return false;
+                } else if known.contains_key(var) && known.contains_key(snd) {
+                    let var_val = known.get(var).unwrap();
+                    let snd_val = known.get(snd).unwrap();
+                    known.insert(
+                        fst,
+                        match op {
+                            &"+" => var_val - snd_val,
+                            &"-" => var_val + snd_val,
+                            &"*" => var_val / snd_val,
+                            &"/" => var_val * snd_val,
+                            _ => panic!("unknown op: {op}"),
+                        },
+                    );
+                    return false;
+                } else if known.contains_key(var) && known.contains_key(fst) {
+                    let var_val = known.get(var).unwrap();
+                    let fst_val = known.get(fst).unwrap();
+                    known.insert(
+                        snd,
+                        match op {
+                            &"+" => var_val - fst_val,
+                            &"-" => fst_val - var_val,
+                            &"*" => var_val / fst_val,
+                            &"/" => fst_val / var_val,
+                            _ => panic!("unknown op: {op}"),
+                        },
+                    );
+                    return false;
+                }
+                return true;
+            })
+            .collect();
+        dbg!(known.len());
+    }
+    dbg!(known.get("humn"));
+}
+
