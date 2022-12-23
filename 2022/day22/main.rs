127fb0ec05ef04ef0d125bb3773b18f180965c8b
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-12-24 00:49:33 +0100

    wip(2022/22): try to take field and walls into account

--- 2022/day22/main.rs
+++ 2022/day22/main.rs
@@ -1,11 +1,57 @@
 #[derive(Debug)]
+struct Row {
+    start: usize,
+    length: usize,
+    walls: Vec<usize>,
+}
+
+#[derive(Debug)]
 enum Move {
     Left,
     Right,
     Forward(u32),
 }
 
-fn parse_input(input: &str) -> Vec<Move> {
+fn parse_input(input: &str) -> (Vec<Row>, Vec<Move>) {
+    let (rows_str, moves_str) = input.split_once("\n\n").unwrap();
+    let rows = parse_rows(rows_str);
+    let moves = parse_moves(moves_str);
+
+    (rows, moves)
+}
+
+fn parse_rows(input: &str) -> Vec<Row> {
+    input
+        .split("\n")
+        .map(|line| {
+            let mut bs = line.bytes().peekable();
+            let mut start = 0;
+            while bs.next_if_eq(&b' ').is_some() {
+                start += 1;
+            }
+            let mut length = 0;
+            let mut walls = vec![];
+            while let Some(b) = bs.next() {
+                match b {
+                    b'.' => {}
+                    b'#' => walls.push(length),
+                    _ => {
+                        panic!("huh? {b}")
+                    }
+                }
+                length += 1;
+            }
+
+            Row {
+                start,
+                length,
+                walls,
+            }
+        })
+        .collect()
+}
+
+fn parse_moves(input: &str) -> Vec<Move> {
     let mut moves = vec![];
     for byte in input.bytes() {
         match byte {
@@ -27,21 +73,87 @@ fn parse_input(input: &str) -> Vec<Move> {
     moves
 }
 
-fn part1(moves: &Vec<Move>) -> usize {
-    let mut heading = 0;
-    let directions = [(1, 0), (0, -1), (-1, 0), (0, -1)]; // Right, down, left, up
-    let mut x = 0;
-    let mut y = 0;
+fn part1(input: &(Vec<Row>, Vec<Move>)) -> usize {
+    let (rows, moves) = input;
+    let mut heading = 0; // 0= Right, 1=down, 2=left, 3=up
+    let mut x: usize = rows[0].start;
+    let mut y: usize = 0;
 
     for m in moves {
         match m {
             Move::Left => heading = (heading + 3) % 4,
             Move::Right => heading = (heading + 1) % 4,
             Move::Forward(steps) => {
-                for _ in 0..*steps {
-                    let (dx, dy) = directions[heading];
-                    x += dx;
-                    y += dy;
+                'moving: for _ in 0..*steps {
+                    let (new_x, new_y) = match heading {
+                        1 => {
+                            // down
+                            let mut new_y = y + 1;
+                            if new_y >= rows.len() {
+                                new_y = 0;
+                            }
+                            while x < rows[new_y].start
+                                || x >= rows[new_y].start + rows[new_y].length
+                            {
+                                new_y += 1;
+                                if new_y >= rows.len() {
+                                    new_y = 0;
+                                }
+                            }
+                            (x, new_y)
+                        }
+                        3 => {
+                            // up
+                            let mut new_y = y;
+                            if new_y == 0 {
+                                new_y = rows.len();
+                            }
+                            new_y -= 1;
+                            while x < rows[new_y].start
+                                || x >= rows[new_y].start + rows[new_y].length
+                            {
+                                if new_y == 0 {
+                                    new_y = rows.len();
+                                }
+                                new_y -= 1;
+                            }
+                            (x, new_y)
+                        }
+                        0 => {
+                            // right
+                            let row = &rows[y];
+                            let mut new_x = x + 1;
+                            if new_x >= row.start + row.length {
+                                new_x = row.start;
+                            }
+                            (new_x, y)
+                        }
+                        2 => {
+                            // left
+                            let row = &rows[y];
+                            let mut new_x = x;
+                            if new_x <= row.start {
+                                new_x = row.start + row.length;
+                            }
+                            new_x -= 1;
+                            (new_x, y)
+                        }
+                        _ => {
+                            panic!("heading?!?")
+                        }
+                    };
+                    // dbg!((new_x, new_y));
+                    if new_x < rows[new_y].start {
+                        panic!("low");
+                    }
+                    if new_x >= rows[new_y].start + rows[new_y].length {
+                        panic!("high");
+                    }
+                    if rows[new_y].walls.contains(&new_x) {
+                        break 'moving;
+                    } else {
+                        (x, y) = (new_x, new_y);
+                    }
                 }
             }
         }
@@ -49,7 +161,7 @@ fn part1(moves: &Vec<Move>) -> usize {
 
     dbg!((x, y));
 
-    64
+    1000 * (y + 1) + 4 * (x + 1) + heading
 }
 
 #[test]
@@ -68,7 +180,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let parsed_input = parse_input(&file);
 
     let part1 = part1(&parsed_input);
-    println!("part 1: {}", part1);
+    println!("part 1: {}", part1); // 65206 too low, 103242 & 164074 too high
 
     // let part2 = part2(&parsed_input);
     // println!("part 2: {}", part2);
