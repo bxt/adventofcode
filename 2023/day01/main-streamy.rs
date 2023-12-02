f6de87931af646f1b110db0681d8ed1fa9efc270
Bernhard Häussner <bernhard.haeussner@xing.com>
2023-12-02 22:47:32 +0100

    refactor(2023/01): make use of wildcards more

--- 2023/day01/main-streamy.rs
+++ 2023/day01/main-streamy.rs
@@ -53,136 +53,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: false,
                 })
             }
-            (_, Ok(b'f')) => {
-                state = State::F;
+            (_, Ok(b'\n')) => {
+                state = State::START;
+                results.push(Token::Separator)
             }
-            (_, Ok(b's')) => {
-                state = State::S;
-            }
-            (
-                State::START
-                | State::E
-                | State::EI
-                | State::EIG
-                | State::EIGH
-                | State::F
-                | State::FI
-                | State::FO
-                | State::FOU
-                | State::N
-                | State::NI
-                | State::O
-                | State::SI
-                | State::SE
-                | State::SEVE
-                | State::T
-                | State::TH
-                | State::TW,
-                Ok(b'e'),
-            ) => {
-                state = State::E;
-            }
-            (
-                State::START
-                | State::E
-                | State::EI
-                | State::EIG
-                | State::EIGH
-                | State::F
-                | State::FI
-                | State::FIV
-                | State::FOU
-                | State::N
-                | State::NIN
-                | State::ON
-                | State::S
-                | State::SI
-                | State::SE
-                | State::SEV
-                | State::T
-                | State::TH
-                | State::THR
-                | State::THRE
-                | State::TW,
-                Ok(b'n'),
-            ) => {
-                state = State::N;
-            }
-            (
-                State::START
-                | State::E
-                | State::EI
-                | State::EIG
-                | State::EIGH
-                | State::FI
-                | State::FIV
-                | State::FO
-                | State::FOU
-                | State::N
-                | State::NI
-                | State::NIN
-                | State::O
-                | State::ON
-                | State::S
-                | State::SI
-                | State::SE
-                | State::SEV
-                | State::SEVE
-                | State::T
-                | State::TH
-                | State::THR
-                | State::THRE,
-                Ok(b'o'),
-            ) => {
-                state = State::O;
-            }
-            (
-                State::START
-                | State::E
-                | State::EI
-                | State::EIG
-                | State::F
-                | State::FI
-                | State::FIV
-                | State::FO
-                | State::FOU
-                | State::N
-                | State::NI
-                | State::NIN
-                | State::O
-                | State::ON
-                | State::S
-                | State::SI
-                | State::SE
-                | State::SEV
-                | State::SEVE
-                | State::T
-                | State::TH
-                | State::THR
-                | State::THRE
-                | State::TW,
-                Ok(b't'),
-            ) => {
-                state = State::T;
-            }
-            (State::START, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {}
             (State::E, Ok(b'i')) => {
                 state = State::EI;
-            }
-            (State::E, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::EI, Ok(b'g')) => {
                 state = State::EIG;
             }
-            (State::EI, Ok(b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::EIG, Ok(b'h')) => {
                 state = State::EIGH;
-            }
-            (State::EIG, Ok(b'g' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::EIGH, Ok(b't')) => {
                 state = State::T;
@@ -191,23 +73,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: true,
                 })
             }
-            (State::EIGH, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::F, Ok(b'i')) => {
                 state = State::FI;
             }
             (State::F, Ok(b'o')) => {
                 state = State::FO;
             }
-            (State::F, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::FI, Ok(b'v')) => {
                 state = State::FIV;
-            }
-            (State::FI, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::FIV, Ok(b'e')) => {
                 state = State::E;
@@ -216,17 +89,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: true,
                 })
             }
-            (State::FIV, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::FO, Ok(b'n')) => {
                 state = State::ON;
             }
             (State::FO, Ok(b'u')) => {
                 state = State::FOU;
-            }
-            (State::FO, Ok(b'g' | b'h' | b'i' | b'r' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::FOU, Ok(b'r')) => {
                 state = State::START;
@@ -235,20 +102,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: true,
                 })
             }
-            (State::FOU, Ok(b'g' | b'h' | b'i' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::N, Ok(b'i')) => {
                 state = State::NI;
             }
-            (State::N, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::NI, Ok(b'n')) => {
                 state = State::NIN;
-            }
-            (State::NI, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::NIN, Ok(b'e')) => {
                 state = State::E;
@@ -260,14 +118,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (State::NIN, Ok(b'i')) => {
                 state = State::NI;
             }
-            (State::NIN, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::O, Ok(b'n')) => {
                 state = State::ON;
-            }
-            (State::O, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::ON, Ok(b'e')) => {
                 state = State::E;
@@ -279,17 +131,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (State::ON, Ok(b'i')) => {
                 state = State::NI;
             }
-            (State::ON, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::S, Ok(b'i')) => {
                 state = State::SI;
             }
             (State::S, Ok(b'e')) => {
                 state = State::SE;
-            }
-            (State::S, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::SI, Ok(b'x')) => {
                 state = State::START;
@@ -298,23 +144,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: true,
                 })
             }
-            (State::SI, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w')) => {
-                state = State::START;
-            }
             (State::SE, Ok(b'v')) => {
                 state = State::SEV;
             }
             (State::SE, Ok(b'i')) => {
                 state = State::EI;
             }
-            (State::SE, Ok(b'g' | b'h' | b'r' | b'u' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::SEV, Ok(b'e')) => {
                 state = State::SEVE;
-            }
-            (State::SEV, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::SEVE, Ok(b'n')) => {
                 state = State::N;
@@ -326,31 +163,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (State::SEVE, Ok(b'i')) => {
                 state = State::EI;
             }
-            (State::SEVE, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
-
             (State::T, Ok(b'h')) => {
                 state = State::TH;
             }
             (State::T, Ok(b'w')) => {
                 state = State::TW;
             }
-            (State::T, Ok(b'g' | b'i' | b'r' | b'u' | b'v' | b'x')) => {
-                state = State::START;
-            }
-
             (State::TH, Ok(b'r')) => {
                 state = State::THR;
             }
-            (State::TH, Ok(b'g' | b'h' | b'i' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::THR, Ok(b'e')) => {
                 state = State::THRE;
-            }
-            (State::THR, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
             }
             (State::THRE, Ok(b'e')) => {
                 state = State::E;
@@ -362,9 +185,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (State::THRE, Ok(b'i')) => {
                 state = State::EI;
             }
-            (State::THRE, Ok(b'g' | b'h' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
-                state = State::START;
-            }
             (State::TW, Ok(b'o')) => {
                 state = State::O;
                 results.push(Token::Number {
@@ -372,14 +192,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     was_word: true,
                 })
             }
-            (State::TW, Ok(b'g' | b'h' | b'i' | b'r' | b'u' | b'v' | b'w' | b'x')) => {
+            (_, Ok(b'f')) => {
+                state = State::F;
+            }
+            (_, Ok(b's')) => {
+                state = State::S;
+            }
+            (_, Ok(b'e')) => {
+                state = State::E;
+            }
+            (_, Ok(b'n')) => {
+                state = State::N;
+            }
+            (_, Ok(b'o')) => {
+                state = State::O;
+            }
+            (_, Ok(b't')) => {
+                state = State::T;
+            }
+            (_, Ok(_)) => {
                 state = State::START;
             }
-            (_, Ok(b'\n')) => {
-                state = State::START;
-                results.push(Token::Separator)
-            }
-            (_, Ok(0..=9 | 11..=b'd' | b'j'..=b'm' | b'p' | b'q' | b'y'..)) => {}
             (_, Err(e)) => panic!("{:?}", e),
         }
     }
