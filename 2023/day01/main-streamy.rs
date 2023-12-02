990b3586de78d9373473946cd6e01cef14b4210a
Bernhard Häussner <bernhard.haeussner@xing.com>
2023-12-03 00:26:14 +0100

    refactor(2023/01): stream the tokens

--- 2023/day01/main-streamy.rs
+++ 2023/day01/main-streamy.rs
@@ -38,184 +38,202 @@ enum State {
     TW,
 }
 
+struct Lexer<I> {
+    iter: I,
+    state: State,
+}
+
+impl<I> Iterator for Lexer<I>
+where
+    I: Iterator<Item = Result<u8, std::io::Error>>,
+{
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(byte) = self.iter.next() {
+            match (self.state, byte) {
+                (_, Ok(letter @ b'1'..=b'9')) => {
+                    self.state = State::START;
+                    return Some(Token::Number {
+                        value: letter - b'0',
+                        was_word: false,
+                    });
+                }
+                (_, Ok(b'\n')) => {
+                    self.state = State::START;
+                    return Some(Token::Separator);
+                }
+                (State::E, Ok(b'i')) => {
+                    self.state = State::EI;
+                }
+                (State::EI, Ok(b'g')) => {
+                    self.state = State::EIG;
+                }
+                (State::EIG, Ok(b'h')) => {
+                    self.state = State::EIGH;
+                }
+                (State::EIGH, Ok(b't')) => {
+                    self.state = State::T;
+                    return Some(Token::Number {
+                        value: 8,
+                        was_word: true,
+                    });
+                }
+                (State::F, Ok(b'i')) => {
+                    self.state = State::FI;
+                }
+                (State::F, Ok(b'o')) => {
+                    self.state = State::FO;
+                }
+                (State::FI, Ok(b'v')) => {
+                    self.state = State::FIV;
+                }
+                (State::FIV, Ok(b'e')) => {
+                    self.state = State::E;
+                    return Some(Token::Number {
+                        value: 5,
+                        was_word: true,
+                    });
+                }
+                (State::FO, Ok(b'n')) => {
+                    self.state = State::ON;
+                }
+                (State::FO, Ok(b'u')) => {
+                    self.state = State::FOU;
+                }
+                (State::FOU, Ok(b'r')) => {
+                    self.state = State::START;
+                    return Some(Token::Number {
+                        value: 4,
+                        was_word: true,
+                    });
+                }
+                (State::N, Ok(b'i')) => {
+                    self.state = State::NI;
+                }
+                (State::NI, Ok(b'n')) => {
+                    self.state = State::NIN;
+                }
+                (State::NIN, Ok(b'e')) => {
+                    self.state = State::E;
+                    return Some(Token::Number {
+                        value: 9,
+                        was_word: true,
+                    });
+                }
+                (State::NIN, Ok(b'i')) => {
+                    self.state = State::NI;
+                }
+                (State::O, Ok(b'n')) => {
+                    self.state = State::ON;
+                }
+                (State::ON, Ok(b'e')) => {
+                    self.state = State::E;
+                    return Some(Token::Number {
+                        value: 1,
+                        was_word: true,
+                    });
+                }
+                (State::ON, Ok(b'i')) => {
+                    self.state = State::NI;
+                }
+                (State::S, Ok(b'i')) => {
+                    self.state = State::SI;
+                }
+                (State::S, Ok(b'e')) => {
+                    self.state = State::SE;
+                }
+                (State::SI, Ok(b'x')) => {
+                    self.state = State::START;
+                    return Some(Token::Number {
+                        value: 6,
+                        was_word: true,
+                    });
+                }
+                (State::SE, Ok(b'v')) => {
+                    self.state = State::SEV;
+                }
+                (State::SE, Ok(b'i')) => {
+                    self.state = State::EI;
+                }
+                (State::SEV, Ok(b'e')) => {
+                    self.state = State::SEVE;
+                }
+                (State::SEVE, Ok(b'n')) => {
+                    self.state = State::N;
+                    return Some(Token::Number {
+                        value: 7,
+                        was_word: true,
+                    });
+                }
+                (State::SEVE, Ok(b'i')) => {
+                    self.state = State::EI;
+                }
+                (State::T, Ok(b'h')) => {
+                    self.state = State::TH;
+                }
+                (State::T, Ok(b'w')) => {
+                    self.state = State::TW;
+                }
+                (State::TH, Ok(b'r')) => {
+                    self.state = State::THR;
+                }
+                (State::THR, Ok(b'e')) => {
+                    self.state = State::THRE;
+                }
+                (State::THRE, Ok(b'e')) => {
+                    self.state = State::E;
+                    return Some(Token::Number {
+                        value: 3,
+                        was_word: true,
+                    });
+                }
+                (State::THRE, Ok(b'i')) => {
+                    self.state = State::EI;
+                }
+                (State::TW, Ok(b'o')) => {
+                    self.state = State::O;
+                    return Some(Token::Number {
+                        value: 2,
+                        was_word: true,
+                    });
+                }
+                (_, Ok(b'f')) => {
+                    self.state = State::F;
+                }
+                (_, Ok(b's')) => {
+                    self.state = State::S;
+                }
+                (_, Ok(b'e')) => {
+                    self.state = State::E;
+                }
+                (_, Ok(b'n')) => {
+                    self.state = State::N;
+                }
+                (_, Ok(b'o')) => {
+                    self.state = State::O;
+                }
+                (_, Ok(b't')) => {
+                    self.state = State::T;
+                }
+                (_, Ok(_)) => {
+                    self.state = State::START;
+                }
+                (_, Err(e)) => panic!("{:?}", e),
+            }
+        }
+
+        None
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("day01/input.txt")?;
     let bytes = BufReader::new(file).bytes();
-    let mut state = State::START;
-    let mut results = vec![];
-
-    for byte in bytes {
-        match (state, byte) {
-            (_, Ok(letter @ b'1'..=b'9')) => {
-                state = State::START;
-                results.push(Token::Number {
-                    value: letter - b'0',
-                    was_word: false,
-                })
-            }
-            (_, Ok(b'\n')) => {
-                state = State::START;
-                results.push(Token::Separator)
-            }
-            (State::E, Ok(b'i')) => {
-                state = State::EI;
-            }
-            (State::EI, Ok(b'g')) => {
-                state = State::EIG;
-            }
-            (State::EIG, Ok(b'h')) => {
-                state = State::EIGH;
-            }
-            (State::EIGH, Ok(b't')) => {
-                state = State::T;
-                results.push(Token::Number {
-                    value: 8,
-                    was_word: true,
-                })
-            }
-            (State::F, Ok(b'i')) => {
-                state = State::FI;
-            }
-            (State::F, Ok(b'o')) => {
-                state = State::FO;
-            }
-            (State::FI, Ok(b'v')) => {
-                state = State::FIV;
-            }
-            (State::FIV, Ok(b'e')) => {
-                state = State::E;
-                results.push(Token::Number {
-                    value: 5,
-                    was_word: true,
-                })
-            }
-            (State::FO, Ok(b'n')) => {
-                state = State::ON;
-            }
-            (State::FO, Ok(b'u')) => {
-                state = State::FOU;
-            }
-            (State::FOU, Ok(b'r')) => {
-                state = State::START;
-                results.push(Token::Number {
-                    value: 4,
-                    was_word: true,
-                })
-            }
-            (State::N, Ok(b'i')) => {
-                state = State::NI;
-            }
-            (State::NI, Ok(b'n')) => {
-                state = State::NIN;
-            }
-            (State::NIN, Ok(b'e')) => {
-                state = State::E;
-                results.push(Token::Number {
-                    value: 9,
-                    was_word: true,
-                })
-            }
-            (State::NIN, Ok(b'i')) => {
-                state = State::NI;
-            }
-            (State::O, Ok(b'n')) => {
-                state = State::ON;
-            }
-            (State::ON, Ok(b'e')) => {
-                state = State::E;
-                results.push(Token::Number {
-                    value: 1,
-                    was_word: true,
-                })
-            }
-            (State::ON, Ok(b'i')) => {
-                state = State::NI;
-            }
-            (State::S, Ok(b'i')) => {
-                state = State::SI;
-            }
-            (State::S, Ok(b'e')) => {
-                state = State::SE;
-            }
-            (State::SI, Ok(b'x')) => {
-                state = State::START;
-                results.push(Token::Number {
-                    value: 6,
-                    was_word: true,
-                })
-            }
-            (State::SE, Ok(b'v')) => {
-                state = State::SEV;
-            }
-            (State::SE, Ok(b'i')) => {
-                state = State::EI;
-            }
-            (State::SEV, Ok(b'e')) => {
-                state = State::SEVE;
-            }
-            (State::SEVE, Ok(b'n')) => {
-                state = State::N;
-                results.push(Token::Number {
-                    value: 7,
-                    was_word: true,
-                })
-            }
-            (State::SEVE, Ok(b'i')) => {
-                state = State::EI;
-            }
-            (State::T, Ok(b'h')) => {
-                state = State::TH;
-            }
-            (State::T, Ok(b'w')) => {
-                state = State::TW;
-            }
-            (State::TH, Ok(b'r')) => {
-                state = State::THR;
-            }
-            (State::THR, Ok(b'e')) => {
-                state = State::THRE;
-            }
-            (State::THRE, Ok(b'e')) => {
-                state = State::E;
-                results.push(Token::Number {
-                    value: 3,
-                    was_word: true,
-                })
-            }
-            (State::THRE, Ok(b'i')) => {
-                state = State::EI;
-            }
-            (State::TW, Ok(b'o')) => {
-                state = State::O;
-                results.push(Token::Number {
-                    value: 2,
-                    was_word: true,
-                })
-            }
-            (_, Ok(b'f')) => {
-                state = State::F;
-            }
-            (_, Ok(b's')) => {
-                state = State::S;
-            }
-            (_, Ok(b'e')) => {
-                state = State::E;
-            }
-            (_, Ok(b'n')) => {
-                state = State::N;
-            }
-            (_, Ok(b'o')) => {
-                state = State::O;
-            }
-            (_, Ok(b't')) => {
-                state = State::T;
-            }
-            (_, Ok(_)) => {
-                state = State::START;
-            }
-            (_, Err(e)) => panic!("{:?}", e),
-        }
-    }
+    let lexer = Lexer {
+        iter: bytes,
+        state: State::START,
+    };
 
     let mut first_number_part1 = 0;
     let mut last_number_part1 = 0;
@@ -226,7 +244,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut first_number_sum_part2: u32 = 0;
     let mut last_number_sum_part2: u32 = 0;
 
-    for token in results {
+    for token in lexer {
         match token {
             Token::Number { value, was_word } => {
                 if !was_word && first_number_part1 == 0 {
