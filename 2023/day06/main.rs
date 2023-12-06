c7d985526dbf667644811660ab4acdab85caf0fc
Bernhard Häussner <bernhard.haeussner@xing.com>
2023-12-06 12:32:31 +0100

    feat(2023/06): solve part 2

--- 2023/day06/main.rs
+++ 2023/day06/main.rs
@@ -6,6 +6,10 @@ fn parse_number_list(input: &str) -> Vec<u64> {
         .collect()
 }
 
+fn parse_big_number(input: &str) -> u64 {
+    *parse_number_list(&input.replace(" ", "")).first().unwrap()
+}
+
 fn count_win_options((time, distance): (u64, u64)) -> u64 {
     let mut count = 0;
     for button_time in 0..time {
@@ -24,17 +28,26 @@ fn check_count_win_options() {
     assert_eq!(count_win_options((7, 9)), 4);
     assert_eq!(count_win_options((15, 40)), 8);
     assert_eq!(count_win_options((30, 200)), 9);
+    assert_eq!(count_win_options((71530, 940200)), 71503);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::read_to_string("day06/input.txt")?;
     let mut lines = file.split("\n");
-    let times = parse_number_list(lines.next().unwrap().strip_prefix("Time:").unwrap());
-    let distances = parse_number_list(lines.next().unwrap().strip_prefix("Distance:").unwrap());
+    let times_str = lines.next().unwrap().strip_prefix("Time:").unwrap();
+    let distances_str = lines.next().unwrap().strip_prefix("Distance:").unwrap();
+
+    let times = parse_number_list(times_str);
+    let distances = parse_number_list(distances_str);
     let input = times.into_iter().zip(distances).collect::<Vec<_>>();
 
     let part1 = input.into_iter().map(count_win_options).product::<u64>();
     println!("Part 1: {:?}", part1);
+
+    let time = parse_big_number(times_str);
+    let distance = parse_big_number(distances_str);
+
+    println!("Part 2: {:?}", count_win_options((time, distance)));
 
     Ok(())
 }
