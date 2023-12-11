3f9f552fd52d1f992cfc3b7cb5fca5d2ea48b33f
Bernhard Häussner <bernhard.haeussner@xing.com>
2023-12-11 08:17:02 +0100

    refactor(2023/11): make nice

--- 2023/day11/main.rs
+++ 2023/day11/main.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashSet, ops::Add};
+use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::ops::{Add, Mul};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Coord<T>(T, T);
@@ -11,115 +13,91 @@ impl<T: Add<Output = T>> Add for Coord<T> {
     }
 }
 
-impl Coord<isize> {
-    fn on<'a, T>(self, field: &'a Vec<Vec<T>>) -> Option<&T> {
-        if self.0 < 0 || self.1 < 0 {
-            return None;
-        }
-        let index0 = usize::try_from(self.0).unwrap();
-        let index1 = usize::try_from(self.1).unwrap();
-        (index0 < field.len() && index1 < field[index0].len()).then(|| &field[index0][index1])
+impl<T: Copy + Mul<T, Output = T>> Mul<T> for Coord<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
     }
 }
 
-fn enumerate_field<'a, T>(field: &'a Vec<Vec<T>>) -> impl Iterator<Item = (isize, isize)> + 'a {
-    (0..field.len()).map(|index0| {
-        let line = &field[index0];
-        let index0_isize = isize::try_from(index0).unwrap();
-        let index1_extend = isize::try_from(line.len()).unwrap();
-        (index0_isize, index1_extend)
-    })
+fn manhattan_distance(Coord(y1, x1): Coord<usize>, Coord(y2, x2): Coord<usize>) -> usize {
+    max(y1, y2) - min(y1, y2) + max(x1, x2) - min(x1, x2)
 }
 
-fn manhattan_distance(Coord(y1, x1): Coord<isize>, Coord(y2, x2): Coord<isize>) -> isize {
-    (y2 - y1).abs() + (x2 - x1).abs()
+struct SpaceTimeContinuum {
+    galaxy_positions: Vec<Coord<usize>>,
+    expanded_lines: HashSet<usize>,
+    expanded_columns: HashSet<usize>,
+}
+
+impl From<String> for SpaceTimeContinuum {
+    fn from(value: String) -> Self {
+        let mut galaxy_positions = vec![];
+        let cleaned_enumerated_lines = value
+            .lines()
+            .map(|l| l.trim().as_bytes())
+            .filter(|l| !l.is_empty())
+            .enumerate();
+        for (line_index, bytes) in cleaned_enumerated_lines {
+            for (index, byte) in bytes.iter().enumerate() {
+                match byte {
+                    b'#' => galaxy_positions.push(Coord(line_index, index)),
+                    b'.' => {}
+                    _ => panic!("Not sure what do do with {byte:?}"),
+                }
+            }
+        }
+
+        let max_line = galaxy_positions.iter().map(|g| g.0).max().unwrap();
+        let max_column = galaxy_positions.iter().map(|g| g.1).max().unwrap();
+
+        let mut expanded_lines = HashSet::from_iter(0..max_line);
+        let mut expanded_columns = HashSet::from_iter(0..max_column);
+        for Coord(line, column) in galaxy_positions.iter() {
+            expanded_lines.remove(line);
+            expanded_columns.remove(column);
+        }
+
+        Self {
+            galaxy_positions,
+            expanded_lines,
+            expanded_columns,
+        }
+    }
+}
+
+impl SpaceTimeContinuum {
+    fn expand(&self, times: usize) -> Vec<Coord<usize>> {
+        self.galaxy_positions
+            .iter()
+            .map(|&coord| {
+                let Coord(line, col) = coord;
+                let line_expansion = self.expanded_lines.iter().filter(|&&l| l < line).count();
+                let col_expansion = self.expanded_columns.iter().filter(|&&c| c < col).count();
+                coord + Coord(line_expansion, col_expansion) * (times - 1)
+            })
+            .collect()
+    }
+}
+
+fn total_distances(galaxy_positions: Vec<Coord<usize>>) -> usize {
+    let mut sum = 0;
+    for &g1 in galaxy_positions.iter() {
+        for &g2 in galaxy_positions.iter() {
+            sum += manhattan_distance(g1, g2);
+        }
+    }
+    sum / 2
 }
 
 fn main() -> () {
     let file = std::fs::read_to_string("day11/input.txt").unwrap();
+    let space_time_continuum = SpaceTimeContinuum::from(file);
 
-    let field = file
-        .lines()
-        .map(|l| l.trim().as_bytes())
-        .filter(|l| !l.is_empty())
-        .map(|bytes| {
-            bytes
-                .iter()
-                .map(|byte| match byte {
-                    b'#' => true,
-                    b'.' => false,
-                    _ => panic!("Not sure what do do with {byte:?}"),
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-
-    let mut expanded_lines = HashSet::new();
-    for (line_index, line_length) in enumerate_field(&field) {
-        let no_galaxies = !(0..line_length)
-            .map(|index| {
-                let coord = Coord(line_index, index);
-                coord.on(&field).unwrap()
-            })
-            .any(|&b| b);
-        if no_galaxies {
-            expanded_lines.insert(line_index);
-        }
-    }
-
-    let mut expanded_columns = HashSet::new();
-    let (_, line_length) = enumerate_field(&field).next().unwrap();
-    for index in (0..line_length) {
-        let no_galaxies = !enumerate_field(&field)
-            .map(|(line_index, _)| {
-                let coord = Coord(line_index, index);
-                coord.on(&field).unwrap()
-            })
-            .any(|&b| b);
-        if no_galaxies {
-            expanded_columns.insert(index);
-        }
-    }
-
-    let part1 = expanded_distances(&field, &expanded_lines, &expanded_columns, 2);
+    let part1 = total_distances(space_time_continuum.expand(2));
     println!("Part 1: {:?}", part1);
-    let part2 = expanded_distances(&field, &expanded_lines, &expanded_columns, 1000000);
+
+    let part2 = total_distances(space_time_continuum.expand(1000000));
     println!("Part 2: {:?}", part2);
-}
-
-fn expanded_distances(
-    field: &Vec<Vec<bool>>,
-    expanded_lines: &HashSet<isize>,
-    expanded_columns: &HashSet<isize>,
-    times: isize,
-) -> isize {
-    let mut galaxy_positions = vec![];
-    for (line_index, line_length) in enumerate_field(&field) {
-        for index in 0..line_length {
-            let coord = Coord(line_index, index);
-            let &value = coord.on(&field).unwrap();
-            if value {
-                let expansion = Coord(
-                    isize::try_from(expanded_lines.iter().filter(|&&l| l < line_index).count())
-                        .unwrap()
-                        * (times - 1),
-                    isize::try_from(expanded_columns.iter().filter(|&&c| c < index).count())
-                        .unwrap()
-                        * (times - 1),
-                );
-                galaxy_positions.push(coord + expansion)
-            }
-        }
-    }
-
-    let total_distances = galaxy_positions
-        .iter()
-        .flat_map(|&g1| {
-            galaxy_positions
-                .iter()
-                .map(move |&g2| manhattan_distance(g1, g2))
-        })
-        .sum::<isize>()
-        / 2;
-    total_distances
 }
