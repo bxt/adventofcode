b657d144f14f7f5f2ff31b1ec90e61768a695cc4
Bernhard Häussner <bernhard.haeussner@xing.com>
2023-12-19 01:31:27 +0100

    refactor(2023/18): more efficient solution for part 1

--- 2023/day18/main.rs
+++ 2023/day18/main.rs
@@ -1,38 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    iter::empty,
-    ops::Add,
-    str::FromStr,
-    vec,
-};
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct Coord<T>(T, T);
-
-impl<T: Add<Output = T>> Add for Coord<T> {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self::Output {
-        Self(self.0 + other.0, self.1 + other.1)
-    }
-}
-
-impl Coord<isize> {
-    fn on<'a, T>(self, field: &'a [&'a [T]]) -> Option<&T> {
-        if self.0 < 0 || self.1 < 0 {
-            return None;
-        }
-        let index0 = usize::try_from(self.0).unwrap();
-        let index1 = usize::try_from(self.1).unwrap();
-        (index0 < field.len() && index1 < field[index0].len()).then(|| &field[index0][index1])
-    }
-}
-
-impl From<Coord<usize>> for Coord<isize> {
-    fn from(Coord(y, x): Coord<usize>) -> Self {
-        Coord(isize::try_from(y).unwrap(), isize::try_from(x).unwrap())
-    }
-}
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
@@ -40,17 +6,6 @@ enum Direction {
     L,
     D,
     R,
-}
-
-impl From<Direction> for Coord<isize> {
-    fn from(value: Direction) -> Self {
-        match value {
-            Direction::U => Coord(-1, 0),
-            Direction::L => Coord(0, -1),
-            Direction::D => Coord(1, 0),
-            Direction::R => Coord(0, 1),
-        }
-    }
 }
 
 impl FromStr for Direction {
@@ -67,6 +22,20 @@ impl FromStr for Direction {
     }
 }
 
+fn is_counterclockwise(from: Direction, to: Direction) -> bool {
+    let all_directions = [Direction::U, Direction::L, Direction::D, Direction::R];
+    let direction_index = all_directions.iter().position(|&d| d == from).unwrap();
+    to == all_directions[(direction_index + 1) % all_directions.len()]
+}
+
+#[test]
+fn check_is_counterclockwise() {
+    assert_eq!(is_counterclockwise(Direction::L, Direction::D), true);
+    assert_eq!(is_counterclockwise(Direction::D, Direction::R), true);
+    assert_eq!(is_counterclockwise(Direction::D, Direction::L), false);
+    assert_eq!(is_counterclockwise(Direction::R, Direction::D), false);
+}
+
 fn main() -> () {
     let file = std::fs::read_to_string("day18/input.txt").unwrap();
 
@@ -76,49 +45,29 @@ fn main() -> () {
         .filter(|l| !l.is_empty())
         .map(|line| {
             let (direction_str, rest) = line.split_once(" ").unwrap();
-            let (distance_str, rest) = rest.split_once(" ").unwrap();
+            let (distance_str, _) = rest.split_once(" ").unwrap();
             let direction: Direction = direction_str.parse().unwrap();
             let distance: isize = distance_str.parse().unwrap();
-            (direction, distance, rest)
+            (direction, distance)
         })
         .collect::<Vec<_>>();
 
-    let mut trench = HashMap::new();
-    let mut position = Coord(0, 0);
-
-    for (direction, distance, color) in instructions {
-        for _ in 0..distance {
-            trench.insert(position, color);
-            position = position + direction.into();
-        }
-    }
-
-    let min_y = trench.keys().map(|c| c.0).min().unwrap();
-    let max_y = trench.keys().map(|c| c.0).max().unwrap();
-    let min_x = trench.keys().map(|c| c.1).min().unwrap();
-    let max_x = trench.keys().map(|c| c.1).max().unwrap();
-
-    dbg!(min_y);
-    dbg!(min_y);
-    dbg!(max_x);
-    dbg!(max_x);
-
+    let mut x_position = 0;
     let mut lagoon_size = 0;
 
-    for y in min_y..=max_y {
-        let mut is_in_lagoon = false;
-        for x in min_x..=max_x {
-            let position = Coord(y, x);
-            if trench.contains_key(&position) {
-                lagoon_size += 1;
-                if trench.contains_key(&(position + Direction::D.into())) {
-                    is_in_lagoon = !is_in_lagoon;
-                }
-            } else {
-                if is_in_lagoon {
-                    lagoon_size += 1;
-                }
-            }
+    for index in 0..instructions.len() {
+        let (direction, distance) = instructions[index];
+        let (prev_direction, _) =
+            instructions[(index + instructions.len() - 1) % instructions.len()];
+        let (next_direction, _) = instructions[(index + 1) % instructions.len()];
+        let outer_distance = distance
+            + isize::from(!is_counterclockwise(prev_direction, direction))
+            - isize::from(is_counterclockwise(direction, next_direction));
+        match direction {
+            Direction::U => lagoon_size += outer_distance * x_position,
+            Direction::L => x_position += outer_distance,
+            Direction::D => lagoon_size -= outer_distance * x_position,
+            Direction::R => x_position -= outer_distance,
         }
     }
 
