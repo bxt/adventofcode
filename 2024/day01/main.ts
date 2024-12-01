3f801a13cd1d3642b11ec535755f4a9639341453
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-01 22:48:09 +0100

    feat(2024/01): implement part 2

--- 2024/day01/main.ts
+++ 2024/day01/main.ts
@@ -1,15 +1,33 @@
-// run with `deno run --allow-read main.ts`
+// run with `deno run --allow-read=input.txt main.ts`
 
 const file = await Deno.readTextFile("input.txt");
 
-const numberPairs = file.split("\n").filter(line => line).map((line) =>
-  line.split(/ +/).map((n) => parseInt(n, 10))
+const numberPairs = file
+  .split("\n")
+  .filter((line) => line)
+  .map((line) => line.split(/ +/).map((n) => parseInt(n, 10)));
+
+const [left, right] = [0, 1].map((i) =>
+  numberPairs.map((pair) => pair[i]).sort((a, b) => a - b)
 );
 
-const sorted = [0, 1].map((i) => numberPairs.map((pair) => pair[i]).sort((a, b) => a - b));
-
-const part1 = numberPairs.map((_, index) => {
-  return Math.abs(sorted[0][index] - sorted[1][index]);
-}).reduce((accumulator, current) => accumulator + current, 0);
+const part1 = numberPairs
+  .map((_, index) => Math.abs(left[index] - right[index]))
+  .reduce((accumulator, current) => accumulator + current, 0);
 
 console.log(`Part 1: ${part1}`);
+
+let part2 = 0;
+let rightIndex = 0;
+
+for (const leftNumber of left) {
+  while (right[rightIndex] < leftNumber) {
+    rightIndex++;
+  }
+  while (right[rightIndex] === leftNumber) {
+    part2 += leftNumber;
+    rightIndex++;
+  }
+}
+
+console.log(`Part 2: ${part2}`);
