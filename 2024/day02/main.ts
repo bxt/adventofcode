7bb58c2eaf17d679875521a0ae29111c611865c7
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-02 11:29:26 +0100

    feat(2024/02): solve part 2

--- 2024/day02/main.ts
+++ 2024/day02/main.ts
@@ -7,7 +7,7 @@ const reports = file
   .filter((line) => line)
   .map((line) => line.split(/ +/).map((n) => parseInt(n, 10)));
 
-const part1 = reports.filter(report => {
+const isSafe = (report: number[]) => {
   let ascending = true;
   let descending = true;
   let diffOkay = true;
@@ -16,9 +16,25 @@ const part1 = reports.filter(report => {
     const absoluteDiff = Math.abs(diff);
     ascending = ascending && diff >= 0;
     descending = descending && diff <= 0;
-    diffOkay = diffOkay && (absoluteDiff >= 1 && absoluteDiff <= 3);
+    diffOkay = diffOkay && absoluteDiff >= 1 && absoluteDiff <= 3;
   }
   return (ascending || descending) && diffOkay;
-}).length;
+};
+
+const part1 = reports.filter(isSafe).length;
 
 console.log(`Part 1: ${part1}`);
+
+const part2 = reports.filter((report) => {
+  if (isSafe(report)) return true;
+  for (let i = 0; i < report.length; i++) {
+    const reportWithoutOne = [
+      ...report.slice(0, i),
+      ...report.slice(i + 1, report.length),
+    ];
+    if (isSafe(reportWithoutOne)) return true;
+  }
+  return false;
+}).length;
+
+console.log(`Part 2: ${part2}`);
