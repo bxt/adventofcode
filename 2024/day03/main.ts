5777ec337719c8524f67fdf033827d4ce0c38077
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-03 19:06:37 +0100

    feat(2024/03): implement part 2

--- 2024/day03/main.ts
+++ 2024/day03/main.ts
@@ -2,11 +2,34 @@
 
 const file = await Deno.readTextFile("input.txt");
 
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const regex = /mul\((\d{1,3}),(\d{1,3})\)|(do)(n't)?\(\)/g;
 
-const part1 = file.matchAll(regex).map((match) => {
-  const [_, a, b] = match;
-  return parseInt(a, 10) * parseInt(b, 10);
-}).reduce((acc, curr) => acc + curr, 0);
+const instructions = Array.from(
+  file.matchAll(regex).map((match) => {
+    const [_, a, b, theDo, nt] = match;
+    if (theDo) {
+      return { kind: "do" as const, enable: !nt };
+    } else {
+      return { kind: "mul" as const, value: parseInt(a, 10) * parseInt(b, 10) };
+    }
+  })
+);
+
+const part1 = instructions
+  .filter((i) => i.kind === "mul")
+  .reduce((acc, i) => acc + i.value, 0);
 
 console.log(`Part 1: ${part1}`);
+
+let part2 = 0;
+let enabled = true;
+
+for (const i of instructions) {
+  if (i.kind === "mul") {
+    if (enabled) part2 += i.value;
+  } else {
+    enabled = i.enable;
+  }
+}
+
+console.log(`Part 2: ${part2}`);
