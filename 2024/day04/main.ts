a22e97d2bebc2ce446b06baafb6d254ed9e39088
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-04 18:33:12 +0100

    feat(2024/04): solve part 2

--- 2024/day04/main.ts
+++ 2024/day04/main.ts
@@ -6,22 +6,27 @@ const file = await Deno.readTextFile("input.txt");
 
 const lines = file.split("\n").filter((line) => line);
 
-const directions = [
+const fourDirections = [
   [1, 0],
   [0, 1],
   [-1, 0],
   [0, -1],
+];
+
+const diagonalDirections = [
   [1, 1],
   [-1, 1],
   [1, -1],
   [-1, -1],
 ];
 
+const eightDirections = [...fourDirections, ...diagonalDirections];
+
 let part1 = 0;
 
 for (let line = 0; line < lines.length; line++) {
   for (let index = 0; index < lines[line].length; index++) {
-    withNextDirection: for (const direction of directions) {
+    withNextDirection: for (const direction of eightDirections) {
       for (let wordIndex = 0; wordIndex < WORD.length; wordIndex++) {
         const searchLine = line + direction[0] * wordIndex;
         const searchIndex = index + direction[1] * wordIndex;
@@ -35,3 +40,19 @@ for (let line = 0; line < lines.length; line++) {
 }
 
 console.log(`Part 1: ${part1}`);
+
+let part2 = 0;
+
+for (let line = 0; line < lines.length; line++) {
+  for (let index = 0; index < lines[line].length; index++) {
+    if (lines[line][index] !== "A") continue;
+    const [se, ne, sw, nw] = diagonalDirections.map(([dl, di]) => {
+      return lines[line + dl]?.[index + di];
+    });
+    if (!((se === "M" && nw === "S") || (se === "S" && nw === "M"))) continue;
+    if (!((ne === "M" && sw === "S") || (ne === "S" && sw === "M"))) continue;
+    part2++;
+  }
+}
+
+console.log(`Part 2: ${part2}`);
