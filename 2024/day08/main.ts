0cb334c4555f3f8a867d39f5917fcedd7bdc7aba
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-08 16:21:14 +0100

    refactor(2024/08): move out common loop logic

--- 2024/day08/main.ts
+++ 2024/day08/main.ts
@@ -26,45 +26,49 @@ parsedInput.forEach((line, y) => {
   }
 });
 
-const antinodePositions = new Set<string>();
-
 const isInBounds = ([x, y]: Position) => {
-  return (x >= 0 && y >= 0 && x < parsedInput[0].length && y < parsedInput.length);
-}
+  return (
+    x >= 0 && y >= 0 && x < parsedInput[0].length && y < parsedInput.length
+  );
+};
 
 const subtract = ([x1, y1]: Position, [x2, y2]: Position): Position => {
   return [x1 - x2, y1 - y2];
-}
+};
 
-for (const [_frequency, positions] of Object.entries(antennasByFrequency)) {
-  for (const position of positions) {
-    for (const other of positions) {
-      if (position === other) continue;
-      const delta = subtract(other, position);
-      const antinode = subtract(position, delta);
-      if (isInBounds(antinode)) {
-        antinodePositions.add(antinode.toString());
+const forEachAntennaPair = (
+  callback: (position: Position, delta: Position) => void
+) => {
+  for (const [_frequency, positions] of Object.entries(antennasByFrequency)) {
+    for (const position of positions) {
+      for (const other of positions) {
+        if (position === other) continue;
+        const delta = subtract(other, position);
+        callback(position, delta);
       }
     }
   }
-}
+};
+
+const antinodePositions = new Set<string>();
+
+forEachAntennaPair((position, delta) => {
+  const antinode = subtract(position, delta);
+  if (isInBounds(antinode)) {
+    antinodePositions.add(antinode.toString());
+  }
+});
 
 console.log(`Part 1: ${antinodePositions.size}`);
 
 const harmonicAntinodePositions = new Set<string>();
 
-for (const [_frequency, positions] of Object.entries(antennasByFrequency)) {
-  for (const position of positions) {
-    for (const other of positions) {
-      if (position === other) continue;
-      const delta = subtract(other, position);
-      let antinode = position;
-      while (isInBounds(antinode)) {
-        harmonicAntinodePositions.add(antinode.toString());
-        antinode = subtract(antinode, delta);
-      }
-    }
+forEachAntennaPair((position, delta) => {
+  let antinode = position;
+  while (isInBounds(antinode)) {
+    harmonicAntinodePositions.add(antinode.toString());
+    antinode = subtract(antinode, delta);
   }
-}
+});
 
 console.log(`Part 2: ${harmonicAntinodePositions.size}`);
