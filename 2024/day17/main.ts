e1c6dc0d360c09bff3eae6eaaf87807971a55bbd
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-18 01:38:37 +0100

    wip(2024/17): try brute force :)

--- 2024/day17/main.ts
+++ 2024/day17/main.ts
@@ -9,82 +9,102 @@ const match = file.match(regex);
 if (!match) throw new Error("Invalid input");
 const [, aString, bString, cString, programString] = match;
 
-let registerA = parseInt(aString, 10);
-let registerB = parseInt(bString, 10);
-let registerC = parseInt(cString, 10);
 const program = programString.split(",").map((n) => parseInt(n, 10));
 
-console.log(programString);
+const originalRegisterA = parseInt(aString, 10);
+const originalRegisterB = parseInt(bString, 10);
+const originalRegisterC = parseInt(cString, 10);
 
-const resolveCombo = (operand: number) => {
-  if (operand >= 0 && operand <= 3) return operand;
-  if (operand === 4) return registerA;
-  if (operand === 5) return registerB;
-  if (operand === 6) return registerC;
-  throw new Error(`Invalid combo operand: ${operand}`);
+const runWithA = (a: number): number[] => {
+  let ic = 0;
+
+  const resolveCombo = (operand: number) => {
+    if (operand >= 0 && operand <= 3) return operand;
+    if (operand === 4) return registerA;
+    if (operand === 5) return registerB;
+    if (operand === 6) return registerC;
+    throw new Error(`Invalid combo operand: ${operand}`);
+  };
+
+  let registerA = a;
+  let registerB = originalRegisterB;
+  let registerC = originalRegisterC;
+
+  const output = [];
+
+  while (ic < program.length && ic >= 0 && output.length < program.length) {
+    const opCode = program[ic];
+    const operand = program[ic + 1];
+    switch (opCode) {
+      case 0: {
+        // adv
+        registerA >>= resolveCombo(operand);
+        ic += 2;
+        break;
+      }
+      case 1: {
+        // bxl
+        registerB ^= operand;
+        ic += 2;
+        break;
+      }
+      case 2: {
+        // bst
+        registerB = resolveCombo(operand) % 8;
+        ic += 2;
+        break;
+      }
+      case 3: {
+        // jnz
+        if (registerA !== 0) {
+          ic = operand;
+        } else {
+          ic += 2;
+        }
+        break;
+      }
+      case 4: {
+        // bxc
+        registerB ^= registerC;
+        ic += 2;
+        break;
+      }
+      case 5: {
+        // out
+        output.push(resolveCombo(operand) % 8);
+        ic += 2;
+        break;
+      }
+      case 6: {
+        // bdv
+        registerB = registerA >> resolveCombo(operand);
+        ic += 2;
+        break;
+      }
+      case 7: {
+        // cdv
+        registerC = registerA >> resolveCombo(operand);
+        ic += 2;
+        break;
+      }
+    }
+  }
+
+  return output;
 };
 
-let ic = 0;
+console.log(`Part 1: ${runWithA(originalRegisterA).join(",")}`);
 
-const output = [];
-
-while (ic < program.length && ic >= 0) {
-  console.log(ic, registerA, registerB, registerC);
-  const opCode = program[ic];
-  const operand = program[ic + 1];
-  switch (opCode) {
-    case 0: { // adv
-      console.log("adv", resolveCombo(operand));
-      registerA >>= resolveCombo(operand);
-      ic += 2;
-      break;
-    }
-    case 1: { // bxl
-      console.log("bxl", operand);
-      registerB ^= operand;
-      ic += 2;
-      break;
-    }
-    case 2: { // bst
-      console.log("bst", resolveCombo(operand));
-      registerB = resolveCombo(operand) % 8;
-      ic += 2;
-      break;
-    }
-    case 3: { // jnz
-      console.log("jnz", registerA, operand);
-      if (registerA !== 0) {
-        ic = operand;
-      } else {
-        ic += 2;
-      }
-      break;
-    }
-    case 4: { // bxc
-      console.log("bxc");
-      registerB ^= registerC;
-      ic += 2;
-      break;
-    }
-    case 5: { // out
-      console.log("out", resolveCombo(operand));
-      output.push(resolveCombo(operand) % 8);
-      ic += 2;
-      break;
-    }
-    case 6: { // bdv
-      console.log("bdv", resolveCombo(operand));
-      registerB = registerA >> resolveCombo(operand);
-      ic += 2;
-      break;
-    }
-    case 7: { // cdv
-      console.log("cdv", resolveCombo(operand));
-      registerC = registerA >> resolveCombo(operand);
-      ic += 2;
+for (let a = 0; true; a++) {
+  const output = runWithA(a);
+  if (output.length === program.length) {
+    if (output.every((n, i) => n === program[i])) {
+      console.log(`Part 2: ${a}`);
       break;
     }
   }
-}
 
-console.log(`Part 1: ${output.join(",")}`);
+  if (a % 10000000 === 0) {
+    console.log(a);
+  }
+}
