cce6c19ebdc7216c5c94ca39bbfaffd73c7174a6
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-19 21:47:27 +0100

    feat/2024/18): solve part 2

--- 2024/day18/main.ts
+++ 2024/day18/main.ts
@@ -3,22 +3,6 @@
 type Position = readonly [number, number];
 
 const MAX = 70;
-// const MAX = 6;
-
-// const bytes = [
-//   [5, 4],
-//   [4, 2],
-//   [4, 5],
-//   [3, 0],
-//   [2, 1],
-//   [6, 3],
-//   [2, 4],
-//   [1, 5],
-//   [0, 6],
-//   [3, 3],
-//   [2, 6],
-//   [5, 1],
-// ];
 
 const parse = (input: string): Position[] => {
   return input
@@ -57,51 +41,47 @@ const startPosition: Position = [0, 0];
 
 const endPosition: Position = [MAX, MAX];
 
-let frontier = [startPosition];
-let steps = 0;
+const findPath = (blocked: Position[]): number | undefined => {
+  let frontier = [startPosition];
+  let steps = 0;
 
-const fistKiloByte = new Set(parsedInput.slice(0, 1024).map((p) => p.toString()));
+  const blockedSet = new Set(blocked.map((p) => p.toString()));
 
-const seen = new Set([startPosition.toString()]);
+  const seen = new Set([startPosition.toString()]);
 
-while (true) {
-  steps++;
+  while (true) {
+    steps++;
 
-  frontier = frontier.flatMap((position) =>
-    fourDirections.map((direction) => add(position, direction))
-  );
-  frontier = frontier.filter(isInBounds);
-  frontier = frontier.filter(
-    (position) => !fistKiloByte.has(position.toString())
-  );
-  frontier = frontier.filter(
-    (position, index) =>
-      frontier.findLastIndex((p) => equals(p, position)) === index
-  );
-  frontier = frontier.filter((position) => !seen.has(position.toString()));
+    frontier = frontier
+      .flatMap((position) =>
+        fourDirections.map((direction) => add(position, direction))
+      )
+      .filter(isInBounds)
+      .filter((position) => !blockedSet.has(position.toString()))
+      .filter((position) => !seen.has(position.toString()));
 
-  frontier.forEach((position) => seen.add(position.toString()));
+    frontier = frontier.filter(
+      (position, index) =>
+        frontier.findLastIndex((p) => equals(p, position)) === index
+    );
 
-  // Deno.stdout.writeSync(new TextEncoder().encode(steps.toString()));
-  // Deno.stdout.writeSync(new TextEncoder().encode('\n'));
+    frontier.forEach((position) => seen.add(position.toString()));
 
-  // for (let x = 0; x <= MAX; x++) {
-  //   for (let y = 0; y <= MAX; y++) {
-  //     if (frontier.find((p) => equals(p, [x, y]))) {
-  //       Deno.stdout.writeSync(new TextEncoder().encode('O'));
-  //     } else if (seen.has([x, y].toString())) {
-  //       Deno.stdout.writeSync(new TextEncoder().encode('x'));
-  //     } else if (fistKiloByte.has([x, y].toString())) {
-  //       Deno.stdout.writeSync(new TextEncoder().encode('#'));
-  //     } else {
-  //       Deno.stdout.writeSync(new TextEncoder().encode('.'));
-  //     }
-  //   }
-  //   Deno.stdout.writeSync(new TextEncoder().encode('\n'));
-  // }
-  // Deno.stdout.writeSync(new TextEncoder().encode('\n'));
+    if (frontier.some((p) => equals(p, endPosition))) break;
+    if (frontier.length === 0) return undefined;
+  }
 
-  if (frontier.some((p) => equals(p, endPosition))) break;
+  return steps;
+};
+
+const firstKilobyte = 1024;
+
+console.log(`Part 1: ${findPath(parsedInput.slice(0, firstKilobyte))}`);
+
+for (let i = firstKilobyte; i < parsedInput.length; i++) {
+  const result = findPath(parsedInput.slice(0, i));
+  if (result === undefined) {
+    console.log(`Part 2: ${parsedInput[i - 1]}`);
+    break;
+  }
 }
-
-console.log(`Part 1: ${steps}`);
