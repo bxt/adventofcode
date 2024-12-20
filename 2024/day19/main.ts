592de5c18f82fe231b018d2834dc6efef4893f43
Bernhard Häussner <bernhard.haeussner@xing.com>
2024-12-20 10:08:43 +0100

    feat(2024/19): solve part 2

--- 2024/day19/main.ts
+++ 2024/day19/main.ts
@@ -1,126 +1,37 @@
-import { assertEquals } from "jsr:@std/assert";
-
 const file = await Deno.readTextFile("input.txt");
 
 const [patternsString, designsString] = file.split("\n\n");
 
 const patterns = patternsString.split(", ");
 
-const designs = designsString.split("\n");
+const designs = designsString.split("\n").filter((design) => design);
 
-interface PatternTrie {
-  [key: string]: PatternTrie | undefined;
-}
+const possibilities: Record<string, number> = { "": 1 };
 
-const END: PatternTrie = {};
+const getWays = (design: string): number => {
+  if (possibilities[design] !== undefined) return possibilities[design];
 
-const makeTrie = (patterns: string[]): PatternTrie => {
-  const trie: PatternTrie = {};
+  let ways = 0;
 
   for (const pattern of patterns) {
-    let current = trie;
-
-    for (const char of pattern) {
-      current[char] ??= {};
-      current = current[char];
+    if (design.startsWith(pattern)) {
+      ways += getWays(design.slice(pattern.length));
     }
-
-    current["."] = END;
   }
 
-  return trie;
+  possibilities[design] = ways;
+
+  return ways;
 };
 
-const isPossible = (design: string, trie: PatternTrie): boolean => {
-  return isPossibleInner(design, 0, trie, trie);
-};
+let countPossible = 0;
+let countWays = 0;
 
-const isPossibleInner = (
-  design: string,
-  at: number,
-  trie: PatternTrie,
-  fullTrie: PatternTrie
-): boolean => {
-  // console.log(" ".repeat(at) + "?");
-  // console.log(design, at, trie);
-  if (at === design.length) return trie["."] !== undefined;
+for (const design of designs) {
+  const ways = getWays(design);
+  countWays += ways;
+  if (ways > 0) countPossible++;
+}
 
-  const char = design[at];
-
-  const subTrie = trie[char];
-
-  if (subTrie === undefined) return false;
-
-  const isDirectlyPossible = isPossibleInner(design, at + 1, subTrie, fullTrie);
-
-  if (isDirectlyPossible) return true;
-
-  const canEndHere = subTrie["."] !== undefined;
-
-  if (!canEndHere) return false;
-
-  return isPossibleInner(design, at + 1, fullTrie, fullTrie);
-};
-
-assertEquals(isPossible("a", makeTrie(["a"])), true);
-assertEquals(isPossible("a", makeTrie(["b"])), false);
-assertEquals(isPossible("ab", makeTrie(["a", "b"])), true);
-assertEquals(isPossible("ab", makeTrie(["ab"])), true);
-assertEquals(isPossible("ab", makeTrie(["ac"])), false);
-assertEquals(isPossible("ab", makeTrie(["a"])), false);
-assertEquals(isPossible("ab", makeTrie(["a", "c"])), false);
-assertEquals(isPossible("ababab", makeTrie(["ab", "b"])), true);
-assertEquals(isPossible("ababbab", makeTrie(["ab", "b"])), true);
-assertEquals(isPossible("ababba", makeTrie(["ab", "b"])), false);
-assertEquals(
-  isPossible(
-    "abcdefghijklmnopabcdefghijklmnopabcdefghijklmnop",
-    makeTrie(["abcdefghijklmnop"])
-  ),
-  true
-);
-
-const essential = patterns.filter((pattern) =>
-  !isPossible(pattern, makeTrie(patterns.filter((p) => p !== pattern)))
-);
-
-const possiblePatterns = makeTrie(essential);
-
-assertEquals(
-  isPossible(
-    "wbgrwubbwguurubrwubuguuwbuubgbugrguwgwrg",
-    makeTrie([
-      "w",
-      "wb",
-      "u",
-      "wbgrwu",
-      "bbw",
-      "guur",
-      "ubrwubu",
-      "guuwb",
-      "uub",
-      "gbu",
-      "grguwg",
-      "wrg",
-    ])
-  ),
-  true
-);
-
-const printTrie = (trie: PatternTrie, indent = 0) => {
-  const entries = Object.entries(trie);
-  for (const [key, value] of entries) {
-    if (key === ".") continue;
-    console.log(" ".repeat(indent) + key);
-    if (value !== undefined) printTrie(value, indent + 1);
-  }
-};
-
-printTrie(possiblePatterns);
-
-const possibleDesigns = designs.filter((design) => {
-  console.log(design);
-  return isPossible(design, possiblePatterns);
-});
-
-console.log(`Part 1: ${possibleDesigns.length}`);
+console.log(`Part 1: ${countPossible}`);
+console.log(`Part 2: ${countWays}`);
