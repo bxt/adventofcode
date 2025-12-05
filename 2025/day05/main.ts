7953c0ee1521071fee2aed563ec5ce039515e816
Bernhard Häussner <bernhard.haeussner@xing.com>
2025-12-05 12:05:30 +0100

    feat(2025/05): add solution for both parts

---
+++ 2025/day05/main.ts
@@ -0,0 +1,43 @@
+// run with `deno run --allow-read=input.txt main.ts`
+
+const file = await Deno.readTextFile("input.txt");
+
+const [rangesString, idsString] = file.split("\n\n");
+
+const regex = /(\d+)-(\d+)/;
+
+const ranges = rangesString.trim().split("\n").map((entry) => {
+  const match = regex.exec(entry);
+  if (!match) throw new Error(`Invalid entry: "${entry}"`);
+  const [_, from, to] = match;
+
+  return [parseInt(from, 10), parseInt(to, 10)] as const;
+});
+
+const ids = idsString.trim().split("\n").map((entry) => {
+  return parseInt(entry, 10);
+});
+
+const part1 =
+  ids.filter((id) => ranges.some(([from, to]) => id >= from && id <= to))
+    .length;
+
+console.log(`Part 1: ${part1}`);
+
+const sortedRanges = ranges.slice().sort((a, b) => a[0] - b[0]);
+
+let part2 = 0;
+let processedUntil = sortedRanges[0][0];
+
+for (const [from, to] of sortedRanges) {
+  if (to < processedUntil) continue;
+  if (from < processedUntil) {
+    part2 += to - processedUntil + 1;
+    processedUntil = to + 1;
+  } else {
+    part2 += to - from + 1;
+    processedUntil = to + 1;
+  }
+}
+
+console.log(`Part 2: ${part2}`);
