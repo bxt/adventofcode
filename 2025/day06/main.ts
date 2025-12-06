1179daff4de924abffbd8e3631caad35d2fdb43b
Bernhard Häussner <bernhard.haeussner@xing.com>
2025-12-06 21:15:51 +0100

    refactor(2025/06): write more functional style

--- 2025/day06/main.ts
+++ 2025/day06/main.ts
@@ -1,5 +1,8 @@
 // run with `deno run --allow-read=input.txt main.ts`
 
+const range = (from: number, to: number): number[] =>
+  Array.from({ length: to - from }, (_, i) => i + from);
+
 const file = await Deno.readTextFile("input.txt");
 
 const lines = file.trim().split("\n");
@@ -8,80 +11,52 @@ const problemLines = lines.slice(0, -1);
 const operatorsLine = lines[lines.length - 1];
 
 const parseOperator = (char: string): "+" | "*" => {
-  if (char === "+" || char === "*") {
-    return char;
-  } else {
-    throw new Error(`Unknown operator: ${char}`);
-  }
+  if (char === "+" || char === "*") return char;
+  else throw new Error(`Unknown operator: ${char}`);
 };
 
-const operatorFunctions = {
-  "+": (a: number, b: number) => a + b,
-  "*": (a: number, b: number) => a * b,
-};
+const sum = (numbers: number[]): number => numbers.reduce((a, b) => a + b, 0);
+const prod = (numbers: number[]): number => numbers.reduce((a, b) => a * b, 1);
+const operatorFunctions = { "+": sum, "*": prod };
 
-const parsedOperatorsLine = operatorsLine.matchAll(/[+*]/g).map((match) =>
-  [parseOperator(match[0]), match.index] as const
-).toArray();
+const parsedOperatorsLine = operatorsLine.matchAll(/[+*]/g).map((match) => ({
+  operator: parseOperator(match[0]),
+  operatorPosition: match.index,
+})).toArray();
+
+type OperatorInfo = { operatorIndex: number; operatorPosition: number };
+type NumbersExtractor = (operatorInfo: OperatorInfo) => number[];
+
+const doCephalopodMath = (getNumbers: NumbersExtractor) => {
+  const results = range(0, parsedOperatorsLine.length).map((operatorIndex) => {
+    const { operator, operatorPosition } = parsedOperatorsLine[operatorIndex];
+    const numbers = getNumbers({ operatorIndex, operatorPosition });
+    return operatorFunctions[operator](numbers);
+  });
+  return sum(results);
+};
 
 const parsedProblemLines = problemLines.map((line) =>
   line.matchAll(/\d+/g).map(([numberString]) => parseInt(numberString, 10))
     .toArray()
 );
 
-const doCephalopodMath = (
-  getNumbers: (
-    op: { operatorIndex: number; operatorPosition: number },
-  ) => number[],
-) => {
-  let total = 0;
+const extractNumbersPart1: NumbersExtractor = ({ operatorIndex }) =>
+  parsedProblemLines.map((line) => line[operatorIndex]!);
 
-  for (let i = 0; i < parsedOperatorsLine.length; i++) {
-    const [operator, operatorPosition] = parsedOperatorsLine[i];
+console.log(`Part 1: ${doCephalopodMath(extractNumbersPart1)}`);
 
-    let result = operator === "+" ? 0 : 1;
+const problemLinesMaxLength = Math.max(...problemLines.map((l) => l.length));
+const sentinelOperatorInfo = { operatorPosition: problemLinesMaxLength + 1 };
 
-    for (const number of getNumbers({ operatorIndex: i, operatorPosition })) {
-      result = operatorFunctions[operator](result, number);
-    }
+const extractNumbersPart2: NumbersExtractor = (operatorInfo) => {
+  const { operatorIndex, operatorPosition } = operatorInfo;
+  const { operatorPosition: nextPosition } =
+    parsedOperatorsLine[operatorIndex + 1] || sentinelOperatorInfo;
 
-    total += result;
-  }
-
-  return total;
+  return range(operatorPosition, nextPosition - 1).map((k) =>
+    parseInt(problemLines.map((line) => line[k]).join(""), 10)
+  );
 };
 
-console.log(
-  `Part 1: ${
-    doCephalopodMath(({ operatorIndex }) =>
-      parsedProblemLines.map((line) => line[operatorIndex]!)
-    )
-  }`,
-);
-
-const problemLinesMaxLength = problemLines.reduce(
-  (max, line) => Math.max(max, line.length),
-  0,
-);
-
-console.log(
-  `Part 2: ${
-    doCephalopodMath(({ operatorIndex, operatorPosition }) => {
-      const [_, nextPosition] = parsedOperatorsLine[operatorIndex + 1] ||
-        [null, problemLinesMaxLength + 1];
-      const numbers: number[] = [];
-
-      for (let k = operatorPosition; k < nextPosition - 1; k++) {
-        let nextNumberString = "";
-        for (const problemLine of problemLines) {
-          nextNumberString += problemLine[k];
-        }
-        const nextNumber = parseInt(nextNumberString, 10);
-
-        numbers.push(nextNumber);
-      }
-
-      return numbers;
-    })
-  }`,
-);
+console.log(`Part 2: ${doCephalopodMath(extractNumbersPart2)}`);
