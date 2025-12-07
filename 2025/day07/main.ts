6369ee09f46bcd4228a721f1c758a5006e399301
Bernhard Häussner <bernhard.haeussner@xing.com>
2025-12-07 11:33:58 +0100

    feat(2025/07): solve part 2

--- 2025/day07/main.ts
+++ 2025/day07/main.ts
@@ -9,22 +9,29 @@ const [startLine, ...manifoldLines] = lines;
 const startPosition = startLine.indexOf("S");
 
 let splitCount = 0;
-let beamPositions: Set<number> = new Set([startPosition]);
+let beamPositions: Record<number, number> = { [startPosition]: 1 };
 
 for (const line of manifoldLines) {
-  const newBeamPositions: Set<number> = new Set();
+  const newBeamPositions: Record<number, number> = {};
 
-  for (const position of beamPositions) {
+  for (const [positionString, count] of Object.entries(beamPositions)) {
+    const position = Number(positionString);
     if (line[position] === "^") {
-      newBeamPositions.add(position - 1);
-      newBeamPositions.add(position + 1);
+      newBeamPositions[position - 1] ||= 0;
+      newBeamPositions[position - 1] += count;
+      newBeamPositions[position + 1] ||= 0;
+      newBeamPositions[position + 1] += count;
       splitCount++;
     } else {
-      newBeamPositions.add(position);
+      newBeamPositions[position] ||= 0;
+      newBeamPositions[position] += count;
     }
   }
 
   beamPositions = newBeamPositions;
 }
 
+const beamCount = Object.values(beamPositions).reduce((a, b) => a + b, 0);
+
 console.log(`Part 1: ${splitCount}`);
+console.log(`Part 2: ${beamCount}`);
