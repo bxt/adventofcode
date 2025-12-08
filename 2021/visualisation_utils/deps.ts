664633fad634bd845f65eaf42185bc165452d4ab
Bernhard Häussner <bernhard.haeussner@xing.com>
2025-12-08 21:04:04 +0100

    fix(2021/visualisation_utils): update imagescript from 1.2.9 to 1.3.1 and jsr

--- 2021/visualisation_utils/deps.ts
+++ 2021/visualisation_utils/deps.ts
@@ -1,2 +1,2 @@
 export { chunk } from "jsr:@std/collections@1.1.3"
-export { Frame, GIF } from "https://deno.land/x/imagescript@1.2.9/mod.ts";
+export { Frame, GIF }  from "jsr:@matmen/imagescript@1.3.1";
